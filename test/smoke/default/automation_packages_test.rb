da2ba05e61823c53a57d26c0cd3850ef18be4d36
Eric Hanko <v-erhank@microsoft.com>
2017-06-03 17:51:39 -0700

    package tests are passing

--- test/smoke/default/automation_packages_test.rb
+++ test/smoke/default/automation_packages_test.rb
@@ -1,7 +1,7 @@
-describe command('which python3') do
+describe command('which /usr/local/bin/python3') do
   its('stdout') { should match('/usr/local/bin/python3') }
 end
 
-describe command('which ifuse') do
+describe command('which /usr/local/bin/ifuse') do
   its('stdout') { should match('/usr/local/bin/ifuse') }
 end
