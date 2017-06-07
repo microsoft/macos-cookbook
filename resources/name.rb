781537d842a882c61feedfc5e212eefb1350f565
Eric Hanko <v-erhank@microsoft.com>
2017-06-07 16:10:31 -0700

    Add name resource

--- resources/name.rb
+++ resources/name.rb
@@ -1,7 +1,21 @@
-# name=$1
-# scutil --set ComputerName "$name"
-# scutil --set HostName "$name.local"
-# scutil --set LocalHostName "$name"
-# defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$name"
+require 'defaults'
 
+resource_name :name
+default_action :run
 
+BASE_COMMAND = '/usr/sbin/scutil'.freeze
+SMB_SERVER_PLIST = '/Library/Preferences/SystemConfiguration/com.apple.smb.server'.freeze
+
+property :name, String, name_property: true
+
+action :run do
+  execute BASE_COMMAND do
+    command "#{BASE_COMMAND} --set ComputerName #{new_resource.name}"
+    command "#{BASE_COMMAND} --set LocalHostName #{new_resource.name}.local"
+    command "#{BASE_COMMAND} --set HostName #{new_resource.name}"
+  end
+
+  defaults SMB_SERVER_PLIST do
+    settings 'NetBIOSName' => new_resource.name
+  end
+end
