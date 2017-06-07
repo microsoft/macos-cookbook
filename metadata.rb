099a2ff05afd206e1d48c2a384c518b7bbab23c2
Eric Hanko <v-erhank@microsoft.com>
2017-06-07 13:47:05 -0700

    Update gitignore and fix metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -2,22 +2,10 @@ name 'macos'
 maintainer 'Copyright 2017 Microsoft. All rights reserved.'
 maintainer_email 'apxlab@microsoft.com'
 license 'MIT'
-description 'Installs/Configures macos-cookbook-cookbook'
-long_description 'Installs/Configures macos-cookbook-cookbook'
-version '0.2.1'
-chef_version '~> 13' if respond_to?(:chef_version)
-source_url 'https://office.visualstudio.com'
-issues_url 'https://office.visualstudio.com'
+description 'Installs/Configures macos-cookbook'
+long_description 'Installs/Configures macos-cookbook'
+version '0.2.2'
+chef_version '~> 13.0' if respond_to?(:chef_version)
+source_url 'https://github.com/Microsoft/macos-cookbook'
+issues_url 'https://github.com/Microsoft/macos-cookbook/issues'
 supports 'mac_os_x'
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/macos/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/macos'
