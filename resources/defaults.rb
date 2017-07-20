211aaf13196fee8ce6969e1d8792a49440852639
Jacob Zaval <jazava@microsoft.com>
2017-07-20 15:36:28 -0700

    update defaults resource to handle user property for user settings

--- resources/defaults.rb
+++ resources/defaults.rb
@@ -7,12 +7,19 @@ property :option, String, default: 'write'
 property :read_only, [true, false], default: false
 property :settings, Hash
 property :system, [true, false]
+property :user, String
 
 action :run do
   new_resource.option = 'read' if read_only
   settings.each do |setting, value|
-    execute BASE_COMMAND do
-      command "#{BASE_COMMAND} #{new_resource.option} #{new_resource.domain} #{setting} #{value}"
+    if new_resource.user
+      execute BASE_COMMAND do
+        command "sudo -u #{new_resource.user} #{BASE_COMMAND} #{new_resource.option} /Users/#{new_resource.user}/Library/Preferences/#{new_resource.domain} #{setting} #{value}"
+      end
+    else
+      execute BASE_COMMAND do
+        command "#{BASE_COMMAND} #{new_resource.option} #{new_resource.domain} #{setting} #{value}"
+      end
     end
   end
 end
