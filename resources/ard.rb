fc91d05acec3d15e0ac0ddffd5385ff07bfe5a20
Ryan Dominguez <v-rydom@microsoft.com>
2017-07-27 15:05:59 -0700

    add case

--- resources/ard.rb
+++ resources/ard.rb
@@ -3,13 +3,36 @@ resource_name :ard
 BASE_COMMAND = '/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart'.freeze
 
 property :status, String, name_property: true
-property :settings, Hash
+property :options, Hash
 property :privileges, Hash
+
+action :run do
+  case new_resource.status
+  when 'activate'
+    execute BASE_COMMAND do
+      command "#{BASE_COMMAND} #{new_resource.status}"
+    end
+
+  when 'deactivate'
+    execute BASE_COMMAND do
+      command "#{BASE_COMMAND} #{new_resource.status}"
+    end
+
+  when 'restart'
+    execute BASE_COMMAND do
+      command "#{BASE_COMMAND} #{new_resource.status}"
+    end
+
+  else
+    execute BASE_COMMAND do
+      command "#{BASE_COMMAND}"
+
+    end
 
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart
 -activate -configure -allowAccessFor -allUsers -privs -all -clientopts -setmenuextra -menuextra yes
 
-
+# if not activate, deactivate, or restart, move to -configure
 
 ard 'activate'
 ard 'restart'
