07db75ddfd2d48dc423fb2b0e317a231cb468d25
Eric Hanko <v-erhank@microsoft.com>
2017-07-27 11:56:08 -0700

    type error in macos cookbook

--- resources/defaults.rb
+++ resources/defaults.rb
@@ -12,9 +12,23 @@ property :user, String
 action :run do
   new_resource.option = 'read' if read_only
   settings.each do |setting, value|
-      execute BASE_COMMAND do
-        command "#{BASE_COMMAND} #{new_resource.option} #{new_resource.domain} #{setting} #{value}"
-        user new_resource.user
-      end
+    case value
+    when Array
+      value = "-array #{value}"
+    when Integer
+      value = "-int #{value}"
+    when TrueClass
+      value = '-bool TRUE'
+    when FalseClass
+      value = '-bool FALSE'
+    when Hash
+      value = "-dict #{value}"
+    else
+      raise(Exception)
+    end
+    execute BASE_COMMAND do
+      command "#{BASE_COMMAND} #{new_resource.option} #{new_resource.domain} #{setting} #{value}"
+      user new_resource.user
+    end
   end
 end
