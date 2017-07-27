c4388fba7a11b5d7dc4cebd774e30075eecccf1a
Eric Hanko <v-erhank@microsoft.com>
2017-07-27 15:05:39 -0700

    add float type

--- resources/defaults.rb
+++ resources/defaults.rb
@@ -17,7 +17,8 @@ action :run do
               TrueClass.to_s => '-bool TRUE',
               FalseClass.to_s => '-bool FALSE',
               Hash.to_s => "-dict #{value}",
-              String.to_s => "-string #{value}" }
+              String.to_s => "-string #{value}",
+              Float.to_s => "-float #{vaue}" }
     value = cases[value.class.to_s]
     execute BASE_COMMAND do
       command "#{BASE_COMMAND} #{new_resource.option} #{new_resource.domain} #{setting} #{value}"
