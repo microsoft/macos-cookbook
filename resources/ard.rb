5306bf7bbc2c91d4cf94a29168449790a592c281
Ryan Dominguez <v-rydom@microsoft.com>
2017-07-27 16:08:00 -0700

    add comment for three case options

--- resources/ard.rb
+++ resources/ard.rb
@@ -10,17 +10,17 @@ action :run do
   case new_resource.status
   when 'activate'
     execute BASE_COMMAND do
-      command "#{BASE_COMMAND} #{new_resource.status} -configure -allowAccessFor -allUsers"
+      command "#{BASE_COMMAND} -#{new_resource.status} -configure -allowAccessFor -allUsers"
     end
 
   when 'deactivate'
     execute BASE_COMMAND do
-      command "#{BASE_COMMAND} #{new_resource.status} -configure -access -off"
+      command "#{BASE_COMMAND} -#{new_resource.status} -configure -access -off"
     end
 
   when 'restart'
     execute BASE_COMMAND do
-      command "#{BASE_COMMAND} #{new_resource.status} -agent"
+      command "#{BASE_COMMAND} -#{new_resource.status} -agent"
     end
 
   else
@@ -32,11 +32,11 @@ action :run do
 end
 
 # activate
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart
--activate -configure -allowAccessFor -allUsers -privs -all -clientopts -setmenuextra -menuextra yes
+# sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart
+# -activate -configure -allowAccessFor -allUsers -privs -all -clientopts -setmenuextra -menuextra yes
 
 # deactivate
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate -configure -access -off
+# sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate -configure -access -off
 
 # restart
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent
+# sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent
