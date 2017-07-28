2873ffde12f32b875f59dcfa3441e7a4d53419ee
Eric Hanko <v-erhank@microsoft.com>
2017-07-28 14:57:50 -0700

    fix syntax issues; add allow_access_for

--- resources/ard.rb
+++ resources/ard.rb
@@ -7,10 +7,9 @@ property :uninstall_options, Array, default: ['-files', '-settings', '-prefs']
 property :restart_options, Array, default: ['-agent', '-console', '-menu']
 
 property :users, Array
-
 property :privs, Array, default: ['-all']
 property :access, String, default: '-on'
-property :allow_access, String, default: '-allUsers'
+property :allow_access_for, String, default: '-allUsers'
 property :computerinfo, Array
 property :clientopts, Array
 
@@ -52,16 +51,24 @@ end
 
 action :configure do
   configure_options = []
-  if new_resource.user
+  if new_resource.users
     configure_options.insert(0, "-users #{new_resource.users.join(',')}")
+  end
   if new_resource.privs
     configure_options.insert(0, "-privs #{new_resource.privs.join(' ')}")
+  end
   if new_resource.access
     configure_options.insert(0, "-access #{new_resource.access}")
+  end
+  if new_resource.allow_access_for
+    configure_options.insert(0, "-allowAccessFor #{new_resource.allow_access_for}")
+  end
   if new_resource.computerinfo
     configure_options.insert(0, "-computerinfo #{new_resource.computerinfo.join(' ')}")
+  end
   if new_resource.clientopts
     configure_options.insert(0, "-clientopts #{new_resource.clientopts.join(' ')}")
+  end
   execute BASE_COMMAND do
     command "#{BASE_COMMAND} -configure #{configure_options.join(' ')}"
   end
