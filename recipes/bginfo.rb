e0526de9183ad0902517b1bd04e9c3feb02356b6
Jacob Zaval <jazava@microsoft.com>
2017-09-01 18:42:59 -0700

    set ownership of BGInfo to whomever the autologin user is when the recipe is run

--- recipes/bginfo.rb
+++ recipes/bginfo.rb
@@ -19,7 +19,20 @@ end
 
 execute 'BGInfo Installer' do
   command "#{bginfo_src}/setup.command #{node['macos']['admin_password']}"
-  user    node['macos']['admin_user']
-  creates "/Users/#{node['macos']['admin_user']}/Shared/BGInfo/"
-  action  :run
+  user node['macos']['admin_user']
+end
+
+auto_login_user = shell_out!('defaults read /Library/Preferences/com.apple.loginwindow autoLoginUser').stdout.strip
+
+directory '/Users/Shared/BGInfo' do
+  owner auto_login_user
+  recursive true
+end
+
+bginfo_files = %w(bginfo.command final_bg.gif macstorage.sh storage.rb)
+
+bginfo_files.each do |file|
+  file "/Users/Shared/BGInfo/#{file}" do
+    owner auto_login_user
+  end
 end
