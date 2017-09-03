e7471818337ea23ccb30a11f64002fd3b50fa91d
Jacob Zaval <jazava@microsoft.com>
2017-09-02 19:36:46 -0700

    refactor bginfo_home

--- recipes/bginfo.rb
+++ recipes/bginfo.rb
@@ -1,5 +1,6 @@
 bginfo_repo = 'http://apexlabgit.corp.microsoft.com/mike/BG-Info-Mac.git'
 bginfo_src  = "/Users/#{node['macos']['admin_user']}/bginfo_src"
+bginfo_home = '/Users/Shared/BGInfo'
 include_recipe 'homebrew'
 git 'BGInfo Repo' do
   repository bginfo_repo
@@ -17,6 +18,7 @@ end
 auto_login_user = shell_out!('defaults read /Library/Preferences/com.apple.loginwindow autoLoginUser').stdout.strip
 
 directory '/Users/Shared/BGInfo' do
+directory bginfo_home do
   owner auto_login_user
   recursive true
 end
@@ -25,6 +27,7 @@ bginfo_files = %w(bginfo.command final_bg.gif macstorage.sh storage.rb)
 
 bginfo_files.each do |file|
   file "/Users/Shared/BGInfo/#{file}" do
+  file "#{bginfo_home}/#{file}" do
     owner auto_login_user
   end
 end
