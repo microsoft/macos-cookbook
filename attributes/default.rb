abe36b8ad9c17c8059bc15c8d370b1e6049a1000
Jacob Zaval <jazava@microsoft.com>
2017-09-02 17:32:15 -0700

    set homebrew owner to admin user

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default['macos']['admin_user'] = 'vagrant'
 default['macos']['admin_password'] = 'vagrant'
-default['homebrew']['owner'] = 'exodus'
+default['homebrew']['owner'] = node['macos']['admin_user']
