b0be5479b1f55e7cfbe4b97e9b96a4ce327562fc
Jacob Zaval <jazava@microsoft.com>
2017-09-03 18:10:02 -0700

    determine BGInfo owner lazily

--- recipes/bginfo.rb
+++ recipes/bginfo.rb
@@ -1,24 +1,29 @@
-auto_login_user = shell_out!('defaults read /Library/Preferences/com.apple.loginwindow autoLoginUser').stdout.strip
 bginfo_repo = 'http://apexlabgit.corp.microsoft.com/mike/BG-Info-Mac.git'
-bginfo_src  = "/Users/#{auto_login_user}/bginfo_src"
+bginfo_src  = '/tmp/bginfo_src'
 bginfo_home = '/Users/Shared/BGInfo'
 
-include_recipe 'homebrew'
+ruby_block 'determine BGInfo owner' do
+  block do
+    node.default['bginfo']['owner'] =
+      shell_out!('defaults read /Library/Preferences/com.apple.loginwindow autoLoginUser').stdout.strip
+  end
+end
+
+package 'imagemagick'
+package 'ghostscript'
 
 git 'BGInfo Repo' do
   repository bginfo_repo
   destination bginfo_src
-  user auto_login_user
   action :sync
 end
 
 execute 'BGInfo Installer' do
-  command "#{bginfo_src}/setup.command #{node['macos']['admin_password']}"
-  user auto_login_user
+  command "#{bginfo_src}/setup.command"
 end
 
 directory bginfo_home do
-  owner auto_login_user
+  owner user lazy { node['bginfo']['owner'] }
   recursive true
 end
 
@@ -29,6 +34,6 @@ bginfo_home_contents = %w(bginfo.command
 
 bginfo_home_contents.each do |file|
   file "#{bginfo_home}/#{file}" do
-    owner auto_login_user
+    owner user lazy { node['bginfo']['owner'] }
   end
 end
