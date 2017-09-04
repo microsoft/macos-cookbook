b03266f9dbaca3691a62dbc0dd00be43ea4a4b69
Eric Hanko <v-erhank@microsoft.com>
2017-09-04 16:16:11 -0700

    revert helper.rb

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -1,10 +1,16 @@
-module Xcode
+module PspdfkitCiMacos
   # Helper methods for recipes
   module Helper
     def xcode_installed?
+      # > xcversion installed
+      # 7.3 (/Applications/Xcode.app)
+      #
+      # irb(main):001:0> installed_xcodes = `xcversion installed`.split(/\s+/).reject!.with_index { |_, i| i.even? } || []
+      # => ["(/Applications/Xcode.app)"]
       installed_xcodes = shell_out!('/usr/local/bin/xcversion installed').stdout.split(/\s+/).reject!.with_index { |_, i| i.even? } || []
 
       installed_xcode_versions = installed_xcodes.map do |xcode|
+        # Remove brackets by removing first and last character
         path = xcode[1..-2]
         shell_out!("DEVELOPER_DIR=#{path} xcodebuild -version").stdout.split.last
       end
@@ -14,4 +20,4 @@ module Xcode
   end
 end
 
-::Chef::Resource.send(:include, Xcode::Helper)
+::Chef::Resource.send(:include, PspdfkitCiMacos::Helper)
