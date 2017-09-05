9fc6f45fc905afc2a9ebd42fd8abadd81a456b70
Eric Hanko <v-erhank@microsoft.com>
2017-09-04 23:20:39 -0700

    add power settings to insomnia.rb

--- recipes/insomnia.rb
+++ recipes/insomnia.rb
@@ -1,5 +1,33 @@
-#
-# Cookbook:: .
-# Recipe:: insomnia
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+systemsetup 'keep awake and set time with systemsetup' do
+  set sleep: 0,
+      computersleep: 0,
+      displaysleep: 0,
+      harddisksleep: 0,
+      remoteappleevents: 'On',
+      allowpowerbuttontosleepcomputer: 'Off',
+      waitforstartupafterpowerfailure: 0,
+      restartfreeze: 'On',
+      networktimeserver: 'time.windows.com',
+      timezone: 'America/Los_Angeles'
+end
+
+pmset 'keep awake with /usr/bin/pmset' do
+  settings sleep: 0,
+           hibernatemode: 0,
+           womp: 1, # wake on ethernet magic packet
+           hibernatefile: '/var/vm/sleepimage',
+           ttyskeepawake: 1
+end
+
+defaults '/Library/Preferences/com.apple.PowerManagement' do
+  settings 'DarkWakeBackgroundTasks' => false, # power nap
+           '"Automatic Restart On Power Loss"' => true,
+           '"Display Sleep Timer"' => 0,
+           '"Disk Sleep Timer"' => 0,
+           '"Wake On LAN"' => true,
+           '"System Sleep Timer"' => 0
+end
+
+execute 'defaults -currentHost write com.apple.screensaver idleTime 0' do
+  user node['apex_automation']['test_user']
+end
