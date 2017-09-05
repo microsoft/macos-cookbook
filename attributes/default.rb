3b1de84f7f58418c82b8b4d438ba883703399d80
Jacob Zaval <jazava@microsoft.com>
2017-09-04 22:09:03 -0700

    fix mono version bug

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,6 +7,5 @@ default['macos']['xcode']['build_version'] = '8E2002'
 default['macos']['simulator']['previous_versions_to_install'] = '1'
 
 default['macos']['mono']['package'] = 'MonoFramework-MDK-4.4.2.11.macos10.xamarin.universal.pkg'
-default['macos']['mono']['version'] = '4.2.2'
+default['macos']['mono']['version'] = '4.4.2'
 default['macos']['mono']['checksum'] = 'd8bfbee7ae4d0d1facaf0ddfb70c0de4b1a3d94bb1b4c38e8fa4884539f54e23'
-
