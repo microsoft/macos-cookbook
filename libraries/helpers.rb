bcf0bf252d51a6e77c96fabe14ff14dc1b6258bc
Eric Hanko <v-erhank@microsoft.com>
2017-09-08 14:27:25 -0700

    add explanatory comments

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -2,12 +2,16 @@ module PspdfkitCiMacos
   # Helper methods for recipes
   module Helper
     def xcode_installed?
-      # > xcversion installed
-      # 7.3 (/Applications/Xcode.app)
-      #
-      # irb(main):001:0> installed_xcodes = `xcversion installed`.split(/\s+/).reject!.with_index { |_, i| i.even? } || []
-      # => ["(/Applications/Xcode.app)"]
+
+      # [2] pry(main)> installed_xcodes = `xcversion installed`.split(/\s+/).reject!.with_index { |_, i| i.even? } || []
+      # => ["(/Applications/Xcode.app)", "(/Users/erichanko/projects-local/Xcode-8.3.3.app)"]
       installed_xcodes = shell_out!('/usr/local/bin/xcversion installed').stdout.split(/\s+/).reject!.with_index { |_, i| i.even? } || []
+
+      # [5] pry(main)> installed_xcode_versions = installed_xcodes.map do |xcode|
+      #   [5] pry(main)*   path = xcode[1..-2]
+      #   [5] pry(main)*   `DEVELOPER_DIR=#{path} xcodebuild -version`.split.last
+      #   [5] pry(main)* end
+      # => ["8E2002", "8E3004b"]
 
       installed_xcode_versions = installed_xcodes.map do |xcode|
         # Remove brackets by removing first and last character
@@ -15,6 +19,8 @@ module PspdfkitCiMacos
         shell_out!("DEVELOPER_DIR=#{path} xcodebuild -version").stdout.split.last
       end
 
+      # Take the declared build version and check to see if it already exists in the list of
+      # installed Xcode build versions
       installed_xcode_versions.include?(node['macos']['xcode']['build_version'])
     end
   end
