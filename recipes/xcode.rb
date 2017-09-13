12e7aad64e3b24b015577c8857bfa9ae4b81d624
Jacob Zaval <jazava@microsoft.com>
2017-09-12 20:48:53 -0700

    extract xcversion path to a variable

--- recipes/xcode.rb
+++ recipes/xcode.rb
@@ -1,6 +1,7 @@
 xcode_version = node['macos']['xcode']['version']
 xcode_user = node['macos']['admin_user']
 xcode_path = '/Applications/Xcode.app'
+xcversion = '/usr/local/bin/xcversion'
 
 developer_creds = {
   'XCODE_INSTALL_USER' => data_bag_item('credentials', 'apple_id')['apple_id'],
@@ -11,7 +12,7 @@ gem_package 'xcode-install'
 
 ruby_block 'determine if requested Xcode is already installed' do
   block do
-    xcversion_output = shell_out!('/usr/local/bin/xcversion installed').stdout.split
+    xcversion_output = shell_out!("#{xcversion} installed").stdout.split
     installed_xcodes = xcversion_output.values_at(*xcversion_output.each_index.select(&:even?))
     node.default['macos']['xcode']['already_installed?'] =
       installed_xcodes.include?(node['macos']['xcode']['version'])
@@ -19,13 +20,13 @@ ruby_block 'determine if requested Xcode is already installed' do
 end
 
 execute 'get Xcode versions currently available from Apple' do
-  command lazy { '/usr/local/bin/xcversion update' }
+  command lazy { "#{xcversion} update" }
   environment developer_creds
   not_if { node['macos']['xcode']['already_installed?'] }
 end
 
 execute 'installed requested Xcode' do
-  command lazy { "/usr/local/bin/xcversion install '#{xcode_version}'" }
+  command lazy { "#{xcversion} install '#{xcode_version}'" }
   environment developer_creds
   creates xcode_path
   not_if { node['macos']['xcode']['already_installed?'] }
