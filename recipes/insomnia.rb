2094e3867e4bdb2644869d7d6bb85d79ca58d255
Jacob Zaval <jazava@microsoft.com>
2017-09-12 22:32:54 -0700

    lazily evaluate power user

--- recipes/insomnia.rb
+++ recipes/insomnia.rb
@@ -28,6 +28,14 @@ defaults '/Library/Preferences/com.apple.PowerManagement' do
            '"System Sleep Timer"' => 0
 end
 
-execute 'defaults -currentHost write com.apple.screensaver idleTime 0' do
-  user node['macos']['admin_user']
+ruby_block 'set power settings to autoLoginUser' do
+  block do
+    loginwindow_plist = '/Library/Preferences/com.apple.loginwindow'
+    auto_login_user = "defaults read #{loginwindow_plist} autoLoginUser"
+    node.default['macos']['power']['owner'] = shell_out!(auto_login_user).stdout.strip
+  end
+end
+
+execute 'disable screensaver' do
+  command lazy { "sudo -u #{node['macos']['power']['owner']} defaults -currentHost write com.apple.screensaver idleTime 0" }
 end
