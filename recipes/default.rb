b759e4ebf93e9a2aa7a1c64929f2f8c20a20146e
Jacob Zaval <jazava@microsoft.com>
2017-09-21 12:38:30 -0700

    extract xcode recipe call

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,1 @@
-xcode '9' do
-  ios_simulators %w(11 10 9)
-end
+include_recipe 'macos::xcode'
--- recipes/xcode.rb
+++ recipes/xcode.rb
@@ -1,4 +1,6 @@
-xcode '9'
+xcode '9' do
+  ios_simulators %w(11 10 9)
+end
 
 execute 'enable developer mode' do
   command 'DevToolsSecurity'
