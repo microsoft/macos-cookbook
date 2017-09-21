664baa4b96b1632799792a8495fa96057415f859
Jacob Zaval <jazava@microsoft.com>
2017-09-21 14:22:50 -0700

    add subscribes for simulator, logging changes

--- resources/xcode.rb
+++ resources/xcode.rb
@@ -8,7 +8,7 @@ property :ios_simulators, Array
 begin
   require 'xcode-install'
 rescue LoadError
-  system 'gem install xcode-install --no-ri --no-doc'
+  system 'gem install xcode-install --no-ri --no-doc', out: :close
   Gem.clear_paths
 end
 
@@ -20,33 +20,34 @@ DEVELOPER_CREDENTIALS = {
 }.freeze
 
 action :install do
-  Chef::Log.debug('Reading currently available versions from Apple')
+  Chef::Log.warn('Reading currently available versions from Apple')
   execute 'get Xcode versions currently available from Apple' do
     command "#{BASE_COMMAND} update"
     environment DEVELOPER_CREDENTIALS
     not_if { xcode_already_installed?(new_resource.version) }
   end
 
-  Chef::Log.info("Installing requested Xcode version: #{new_resource.version} at #{new_resource.path}")
+  Chef::Log.warn("Installing requested Xcode version: #{new_resource.version} at #{new_resource.path}")
   execute 'installed requested Xcode' do
     command "#{BASE_COMMAND} install '#{new_resource.version}'"
     environment DEVELOPER_CREDENTIALS
     creates new_resource.path
     not_if { xcode_already_installed?(new_resource.version) }
-    live_stream true
   end
 
   Chef::Log.warn("Installing requested simulator versions: #{new_resource.ios_simulators}")
   new_resource.ios_simulators.each do |simulator|
-    next if highest_eligible_simulator(simulator_list, simulator).nil?
+    next if highest_eligible_sim
+    qulator(simulator_list, simulator).nil?
     execute "Install iOS #{simulator} simulator" do
       semantic_version = highest_eligible_simulator(simulator_list, simulator).join(' ')
       command "#{BASE_COMMAND} simulators --install='#{semantic_version}'"
+      subscribes :run, execute["#{BASE_COMMAND} install '#{new_resource.version}'"], :immediate
       not_if { available_simulator_versions.include?("#{semantic_version} Simulator (installed)") }
     end
   end
 
-  Chef::Log.debug('Accepting Xcode license')
+  Chef::Log.warn('Accepting Xcode license')
   execute 'accept Xcode license' do
     command '/usr/bin/xcodebuild -license accept'
   end
