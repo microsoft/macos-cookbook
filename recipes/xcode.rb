7ad6afd4bbed6692a01af3267465845e9099d15e
Jacob Zaval <jazava@microsoft.com>
2017-09-26 18:45:06 -0700

    extract this out to downstream cookbooks if they need it

--- recipes/xcode.rb
+++ recipes/xcode.rb
@@ -32,19 +32,3 @@ end
 execute 'accept Xcode license' do
   command 'xcodebuild -license accept'
 end
-
-execute 'enable developer mode' do
-  command 'DevToolsSecurity'
-end
-
-ruby_block 'set xcode user to autoLoginUser' do
-  block do
-    loginwindow_plist = '/Library/Preferences/com.apple.loginwindow'
-    auto_login_user = "defaults read #{loginwindow_plist} autoLoginUser"
-    node.default['macos']['xcode']['user'] = shell_out!(auto_login_user).stdout.strip
-  end
-end
-
-execute 'add admin user to Developer group' do
-  command lazy { "dscl . append /Groups/_developer GroupMembership #{node['macos']['xcode']['user']}" }
-end
