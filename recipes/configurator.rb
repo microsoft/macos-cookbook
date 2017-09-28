1379fca23acb6a2933c8ed5b69623fb6b272193b
Eric Hanko <v-erhank@microsoft.com>
2017-09-27 17:14:37 -0700

    rename base command variable; minor reformatting

--- recipes/configurator.rb
+++ recipes/configurator.rb
@@ -1,19 +1,20 @@
-apex_lab_apple_id = data_bag_item('credentials', 'apple_id')
+apex_lab_apple_id   = data_bag_item('credentials', 'apple_id')
 configurator_app_id = '1037126344'
-BASE_COMMAND = '/usr/local/bin/mas'.freeze
+mas_base_command    = '/usr/local/bin/mas'.freeze
+
+include_recipe 'homebrew'
+package 'mas'
 
 execute 'Signin to App Store' do
-  command "#{BASE_COMMAND} signin #{apex_lab_apple_id['apple_id']} #{apex_lab_apple_id['password']}"
+  command "#{mas_base_command} signin #{apex_lab_apple_id['apple_id']} #{apex_lab_apple_id['password']}"
   sensitive true
-  not_if "#{BASE_COMMAND} account"
+  not_if "#{mas_base_command} account"
 end
 
 execute 'Install Configurator' do
-  command "#{BASE_COMMAND} install #{configurator_app_id}"
-  not_if "#{BASE_COMMAND} list | grep 'Apple Configurator'"
+  command "#{mas_base_command} install #{configurator_app_id}"
 end
 
 link '/usr/local/bin/cfgutil' do
   to '/Applications/Apple Configurator 2.app/Contents/MacOS/cfgutil'
-  only_if File.exist?('/Applications/Apple Configurator 2.app')
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,1 @@
-
+include_recipe 'macos::configurator'
