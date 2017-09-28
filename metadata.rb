bbd4b9314413a7dc069b782fd71df26d90a25112
Eric Hanko <v-erhank@microsoft.com>
2017-09-27 17:27:17 -0700

    bump vers to 0.8

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'apxlab@microsoft.com'
 license 'MIT'
 description 'Installs/Configures macos-cookbook'
 long_description 'Installs/Configures macos-cookbook'
-version '0.7'
+version '0.8'
 chef_version '~> 13.0' if respond_to?(:chef_version)
 source_url 'https://github.com/Microsoft/macos-cookbook'
 issues_url 'https://github.com/Microsoft/macos-cookbook/issues'
