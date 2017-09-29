8e73a28939552b0eb17ae27e05ad66d6c241c341
Jacob Zaval <jazava@microsoft.com>
2017-09-29 16:10:24 -0700

    install simulators as a different action

--- resources/xcode.rb
+++ resources/xcode.rb
@@ -1,7 +1,7 @@
 include Xcode::Helper
 
 resource_name :xcode
-default_action %i(setup install)
+default_action %i(setup install_xcode install_simulators)
 
 property :version, String, name_property: true
 property :path, String, default: '/Applications/Xcode.app'
@@ -13,7 +13,7 @@ action :setup do
   end
 end
 
-action :install do
+action :install_xcode do
   CREDENTIALS_DATA_BAG = data_bag_item(:credentials, :apple_id)
 
   DEVELOPER_CREDENTIALS = {
@@ -31,21 +31,23 @@ action :install do
     command "#{BASE_COMMAND} install '#{new_resource.version}'"
     environment DEVELOPER_CREDENTIALS
     not_if { xcode_already_installed?(new_resource.version) }
-    action :nothing
     notifies :run, 'execute[accept license]', :immediately
+    action :nothing
   end
 
   execute 'accept license' do
     command '/usr/bin/xcodebuild -license accept'
     action :nothing
   end
+end
 
+action :install_simulators do
   if new_resource.ios_simulators
     new_resource.ios_simulators.each do |simulator|
       next if simulator.to_i >= included_major_simulator_version.to_i
       semantic_version = highest_eligible_simulator(simulator_list, simulator).join(' ')
 
-      execute "install #{semantic_version} Simulator" do
+      execute 'install Simulator' do
         command "#{BASE_COMMAND} simulators --install='#{semantic_version}'"
         environment DEVELOPER_CREDENTIALS
         not_if { available_simulator_versions.include?("#{semantic_version} Simulator (installed)") }
