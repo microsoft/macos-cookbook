38768f0310cadbca0f4264fa49bfd184da43d770
Jacob Zaval <jazava@microsoft.com>
2017-09-29 15:37:45 -0700

    rewrite simulator block to use execute resource for Chef run output

--- resources/xcode.rb
+++ resources/xcode.rb
@@ -34,16 +34,19 @@ action :install do
     not_if { xcode_already_installed?(new_resource.version) }
   end
 
-  simulators_to_install = new_resource.ios_simulators
-
-  simulators_to_install.each do |simulator|
-    next if highest_eligible_simulator(simulator_list, simulator).nil?
-    semantic_version = highest_eligible_simulator(simulator_list, simulator).join(' ')
-    next if available_simulator_versions.include?("#{semantic_version} Simulator (installed)")
-    system("#{BASE_COMMAND} simulators --install='#{semantic_version}'")
-  end
-
   execute 'accept license' do
     command '/usr/bin/xcodebuild -license accept'
+  end
+
+  if new_resource.ios_simulators
+    new_resource.ios_simulators.each do |simulator|
+      next if simulator.to_i >= included_major_simulator_version.to_i
+      semantic_version = highest_eligible_simulator(simulator_list, simulator).join(' ')
+
+      execute "install #{semantic_version} Simulator" do
+        command "#{BASE_COMMAND} simulators --install='#{semantic_version}'"
+        not_if { available_simulator_versions.include?("#{semantic_version} Simulator (installed)") }
+      end
+    end
   end
 end
