8cfc5ad641d38f787d8bdded47780b66062629b2
Jacob Zaval <jazava@microsoft.com>
2017-09-29 11:46:25 -0700

    delete commented out code

--- recipes/xcode.rb
+++ recipes/xcode.rb
@@ -1,19 +1,3 @@
 xcode '9' do
   ios_simulators %w(11 10 9)
 end
-
-# execute 'enable developer mode' do
-#   command 'DevToolsSecurity'
-# end
-#
-# ruby_block 'set xcode user to autoLoginUser' do
-#   block do
-#     loginwindow_plist = '/Library/Preferences/com.apple.loginwindow'
-#     auto_login_user = "defaults read #{loginwindow_plist} autoLoginUser"
-#     node.default['macos']['xcode']['user'] = shell_out!(auto_login_user).stdout.strip
-#   end
-# end
-#
-# execute 'add admin user to Developer group' do
-#   command lazy { "dscl . append /Groups/_developer GroupMembership #{node['macos']['xcode']['user']}" }
-# end
