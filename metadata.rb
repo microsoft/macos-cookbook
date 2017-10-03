742fb90520687b4329c97881e40812287d94304d
Eammon Hanlon <eammon.hanlon@outlook.com>
2017-10-03 09:35:14 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,15 @@
 name 'macos'
-maintainer 'Copyright 2017 Microsoft. All rights reserved.'
+maintainer 'Microsoft'
 maintainer_email 'apxlab@microsoft.com'
 license 'MIT'
-description 'Installs/Configures macos-cookbook'
-long_description 'Installs/Configures macos-cookbook'
-version '0.8'
+description 'Resources for configuring and provisioning macOS'
+long_description 'Resources for configuring and provisioning macOS'
 chef_version '~> 13.0' if respond_to?(:chef_version)
+version '0.8'
+
 source_url 'https://github.com/Microsoft/macos-cookbook'
 issues_url 'https://github.com/Microsoft/macos-cookbook/issues'
+
 supports 'mac_os_x'
+
 depends 'homebrew'
