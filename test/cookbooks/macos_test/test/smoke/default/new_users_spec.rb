2e1ab41c81471fde2eb3649fcbf8f0b0ec14c680
Eric Hanko <eric.hanko1@gmail.com>
2017-11-28 01:21:06 -0800

    update new_users integration tests

--- test/cookbooks/macos_test/test/smoke/default/new_users_spec.rb
+++ test/cookbooks/macos_test/test/smoke/default/new_users_spec.rb
@@ -1,17 +1,17 @@
 control 'new macOS users' do
-  desc 'they exist with the expected properties'
+  desc 'they exist with the expected characteristics'
 
-  describe user('john') do
+  describe user('randall') do
     it { should exist }
     its('uid') { should eq 503 }
     its('gid') { should eq 20 }
-    its('home') { should eq '/Users/john' }
+    its('home') { should eq '/Users/randall' }
   end
 
-  describe user('john_jr') do
+  describe user('johnny') do
     it { should exist }
     its('uid') { should eq 504 }
     its('gid') { should eq 20 }
-    its('home') { should eq '/Users/john_jr' }
+    its('home') { should eq '/Users/johnny' }
   end
 end
