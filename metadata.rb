7f5a638475f96303640eef7d07b179cdfc8c5ba0
Jacob Zaval <jazava@microsoft.com>
2017-11-28 13:36:24 -0800

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'MIT'
 description 'Resources for configuring and provisioning macOS'
 long_description 'Resources for configuring and provisioning macOS'
 chef_version '~> 13.0' if respond_to?(:chef_version)
-version '0.9.0'
+version '0.10.0'
 
 source_url 'https://github.com/Microsoft/macos-cookbook'
 issues_url 'https://github.com/Microsoft/macos-cookbook/issues'
