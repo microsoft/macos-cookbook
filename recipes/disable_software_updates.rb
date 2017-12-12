4c797a2bef5b82c04eac8246bbe36f571eaec2d6
Jacob Zaval <jazava@microsoft.com>
2017-12-12 10:36:44 -0800

    disable downloads prior to disabling software update check

--- recipes/disable_software_updates.rb
+++ recipes/disable_software_updates.rb
@@ -1,11 +1,11 @@
-plist 'disable automatic software update check' do
-  path '/Library/Preferences/com.apple.SoftwareUpdate.plist'
-  entry 'AutomaticCheckEnabled'
-  value false
-end
-
 plist 'disable automatic software update downloads' do
   path '/Library/Preferences/com.apple.SoftwareUpdate.plist'
   entry 'AutomaticDownload'
+  value false
+end
+
+plist 'disable automatic software update check' do
+  path '/Library/Preferences/com.apple.SoftwareUpdate.plist'
+  entry 'AutomaticCheckEnabled'
   value false
 end
