d5cfbe54cbed0cc30034c5dc7669f6cb557968b5
Jacob Zaval <jazava@microsoft.com>
2017-12-18 15:20:31 -0800

    add spec tests for xcversion module

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'chefspec/berkshelf'
 require_relative '../libraries/macos_user'
 require_relative '../libraries/plist'
 require_relative '../libraries/xcode'
+require_relative '../libraries/xcversion'
 
 RSpec.configure do |config|
   config.platform = 'mac_os_x'
