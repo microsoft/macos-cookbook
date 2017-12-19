3e731a5b7003eeb219e35f96e91c2805debffe59
Jacob Zaval <jazava@microsoft.com>
2017-12-18 16:28:44 -0800

    add spec test for Simulator

---
+++ spec/unit/libraries/xcode_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+include MacOS::Xcode
+
+describe MacOS::Xcode::Simulator do
+  context 'when provided an available list of simulators' do
+    before do
+      allow_any_instance_of(MacOS::Xcode::Simulator).to receive(:available_versions)
+        .and_return(<<-XCVERSION_OUTPUT
+                    Xcode 9.2 (/Applications/Xcode-9.2.app)
+                    iOS 8.1 Simulator (not installed)
+                    iOS 8.2 Simulator (not installed)
+                    iOS 8.3 Simulator (not installed)
+                    iOS 8.4 Simulator (not installed)
+                    iOS 9.0 Simulator (not installed)
+                    iOS 9.1 Simulator (not installed)
+                    iOS 9.2 Simulator (not installed)
+                    iOS 9.3 Simulator (not installed)
+                    iOS 10.0 Simulator (not installed)
+                    iOS 10.1 Simulator (not installed)
+                    tvOS 9.0 Simulator (not installed)
+                    tvOS 9.1 Simulator (not installed)
+                    tvOS 9.2 Simulator (not installed)
+                    tvOS 10.0 Simulator (not installed)
+                    watchOS 2.0 Simulator (not installed)
+                    watchOS 2.1 Simulator (not installed)
+                    watchOS 2.2 Simulator (not installed)
+                    tvOS 10.1 Simulator (not installed)
+                    iOS 10.2 Simulator (not installed)
+                    watchOS 3.1 Simulator (not installed)
+                    iOS 10.3.1 Simulator (not installed)
+                    watchOS 3.2 Simulator (not installed)
+                    tvOS 10.2 Simulator (not installed)
+                    iOS 11.0 Simulator (not installed)
+                    watchOS 4.0 Simulator (not installed)
+                    tvOS 11.0 Simulator (not installed)
+                    tvOS 11.1 Simulator (not installed)
+                    watchOS 4.1 Simulator (not installed)
+                    iOS 11.1 Simulator (not installed)
+                    XCVERSION_OUTPUT
+                   )
+    end
+    it 'returns the latest semantic version of iOS' do
+      s = MacOS::Xcode::Simulator.new('10')
+      expect(s.version).to eq 'iOS 10.3.1'
+    end
+  end
+end
