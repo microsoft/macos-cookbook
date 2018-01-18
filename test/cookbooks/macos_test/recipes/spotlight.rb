107b63642353bb9ae38e51436100b131ef2ae314
Jacob Zaval <jazava@microsoft.com>
2018-01-17 22:15:11 -0800

    create spotlight testing recipe

---
+++ test/cookbooks/macos_test/recipes/spotlight.rb
@@ -0,0 +1,35 @@
+execute 'create test disk collection' do
+  command ['diskutil', 'resizeVolume',
+           'disk0s2', '25g',
+           'jhfs+', 'test_disk1', '1G',
+           'jhfs+', 'TDD2', '1G',
+           'jhfs+', 'Macintosh TD', '1G',
+           'jhfs+', 'TDD-ROM', '700MB']
+end
+
+spotlight 'enabled on boot volume' do
+  volume '/'
+  indexed true
+end
+
+spotlight 'enable indexing on test_disk1' do
+  volume 'test_disk1'
+  indexed true
+end
+
+spotlight 'disable indexing on TDD2' do
+  volume 'TDD2'
+  indexed false
+end
+
+spotlight 'disable indexing and delete metadata store on Macintosh TD' do
+  volume 'Macintosh TD'
+  indexed false
+  erase_store true
+end
+
+spotlight 'disabled indexing and prevent searching index on TDD-ROM' do
+  volume 'TDD-ROM'
+  indexed false
+  allow_search false
+end
