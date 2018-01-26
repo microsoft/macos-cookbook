f659a2da2f032ba0c11482506fb6d35a3cead83c
Jacob Zaval <jazava@microsoft.com>
2018-01-26 13:04:06 -0800

    remove store handling

--- resources/spotlight.rb
+++ resources/spotlight.rb
@@ -3,12 +3,9 @@ default_action %i(set)
 
 property :volume, String, name_property: true
 property :indexed, [true, false], default: true
-property :erase_store, [true, false], default: false
 property :allow_search, [true, false], default: true
 
 action_class do
-  def erase_store?
-    new_resource.erase_store ? '-E' : ''
   end
 
   def allow_search?
@@ -34,6 +31,5 @@ end
 
 action :set do
   execute "turn Spotlight indexing #{state} for #{new_resource.volume}" do
-    command [mdutil, erase_store?, '-i', state, volume_path(new_resource.volume), allow_search?]
   end
 end
