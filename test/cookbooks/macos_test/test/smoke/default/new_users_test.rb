0d4323767db1d6f5d595afc56d6e397e592569d8
Mark John Merin <33106688+mjmerin@users.noreply.github.com>
2018-02-11 17:52:22 -0800

    Feature/useraddtogroup: macos_user new property "groups" (#41)
    * Add smoke test for new property "groups" for macos_user resource
    
    * Add new macos_user that utilizes the new "groups" property
    
    * Add new property for macos_user called "groups"
    
    groups property will take a String or an Array

--- test/cookbooks/macos_test/test/smoke/default/new_users_test.rb
+++ test/cookbooks/macos_test/test/smoke/default/new_users_test.rb
@@ -6,6 +6,7 @@ control 'new macOS users' do
     its('uid') { should eq 503 }
     its('gid') { should eq 20 }
     its('home') { should eq '/Users/randall' }
+    its('groups') { should include 'alpha' }
   end
 
   describe user('johnny') do
@@ -13,11 +14,20 @@ control 'new macOS users' do
     its('uid') { should eq 504 }
     its('gid') { should eq 20 }
     its('home') { should eq '/Users/johnny' }
+    its('groups') { should include 'alpha' }
+    its('groups') { should include 'beta' }
   end
 
   realname_cmd = 'dscl . read /Users/johnny RealName | grep -v RealName | cut -c 2-'
 
   describe command(realname_cmd) do
     its('stdout.strip') { should eq 'Johnny Appleseed' }
+  end
+
+  describe user('paul') do
+    it { should exist }
+    its('uid') { should eq 505 }
+    its('gid') { should eq 20 }
+    its('home') { should eq '/Users/paul' }
   end
 end
