2448f0960157d423f7ef257243e346545fc12316
Eric Hanko <v-erhank@microsoft.com>
2018-02-12 10:58:57 -0800

    remove delivery; fix keep_awake idempotence bug

--- .delivery/build_cookbook/metadata.rb
+++
@@ -1,8 +0,0 @@
-name 'build_cookbook'
-maintainer 'Copyright © 2017 Microsoft. All rights reserved.'
-maintainer_email 'apxlab@microsoft.com'
-license 'mit'
-version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
-
-depends 'delivery-truck'
