3abd15f547f06456245643f802f3f29c1f559a12
Eric Hanko <v-erhank@microsoft.com>
2018-02-20 12:23:55 -0800

    fix syntax error with shellescape

--- resources/defaults.rb
+++ resources/defaults.rb
@@ -11,8 +11,8 @@ action :run do
   new_resource.option = 'read' if new_resource.read_only
   new_resource.settings.each do |setting, value|
     value = "-#{convert_to_string_from_data_type(value)}"
-    execute "#{new_resource.setting} to #{value}" do
-      command [defaults_executable, new_resource.option, Shellwords.shellescape new_resource.domain, Shellwords.shellescape setting, value]
+    execute "#{setting} to #{value}" do
+      command "#{defaults_executable} #{new_resource.option} #{Shellwords.shellescape(new_resource.domain)} #{Shellwords.shellescape(setting)} #{value}"
       user new_resource.user
     end
   end
