c92002a904c9e054de3c6953cae7167d75fccbce
Eric Hanko <v-erhank@microsoft.com>
2018-02-27 20:29:44 -0800

    minor refactors

--- libraries/plist.rb
+++ libraries/plist.rb
@@ -11,29 +11,22 @@ module MacOS
                    String => 'string',
                    Float => 'float' }
       begin
-        value.is_container?(value) ? container_handler(value) : type_map[value.class]
+        value.container?(value) ? container_handler(value) : type_map[value.class]
       rescue
         raise "Tried to set \'#{value}\' of an unsupported data type: #{value.class}"
       end
     end
 
-    def is_container?(value)
-      value.class == Array or value.class == Hash
+    def container?(value)
+      value.class == Array || value.class == Hash
     end
 
     private
 
-    def cli_value_handler(_subcommand, _value)
-      commands.is_a?(Array) ? commands : [commands]
-    end
-
-    def container_handler(value)
-      commands = []
-      value.each do |item|
-        full_cmd = [value.index(item).to_s, entry_type_to_string(item), item]
-        commands.push(full_cmd.join(' '))
-      end
-      commands
+    def container_handler(container_items)
+      index = container_items.index(item).to_s
+      item_type = entry_type_to_string(item)
+      container_items.collect { |item| [index, item_type, item].join(' ') }
     end
   end
 end
--- libraries/plutil.rb
+++ libraries/plutil.rb
@@ -1,14 +1,10 @@
 module MacOS
-  module PlistAssistant
+  module Plutil
     def convert_to_hash(path)
       temp_file = ::Tempfile.new.path
       ::FileUtils.copy(path, temp_file)
       shell_out!(plutil_executable, '-convert', 'xml1', temp_file)
       ::Plist.parse_xml(temp_file)
-    end
-
-    def defaults_executable
-      '/usr/bin/defaults'
     end
 
     def plutil_executable
@@ -17,5 +13,5 @@ module MacOS
   end
 end
 
-Chef::Recipe.include(MacOS::PlistAssistant)
-Chef::Resource.include(MacOS::PlistAssistant)
+Chef::Recipe.include(MacOS::Plutil)
+Chef::Resource.include(MacOS::Plutil)
