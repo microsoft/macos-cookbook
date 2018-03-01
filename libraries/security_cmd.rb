1be643457f9fcf847e0d76c28a6c7e3c4901c3de
Mark John Merin <v-mamer@microsoft.com>
2018-03-01 14:11:58 -0800

    SecurityCommand Class
    Use some functions that are modeled after the actual security command
    such as unlock-keychain, add-certificates, and import.
    
    There is also a install_certificate wrapper that wraps the import and
    add-certificate commands to hide the differences of .p12 and .cer file
    installation from the user.

--- libraries/security_cmd.rb
+++ libraries/security_cmd.rb
@@ -2,6 +2,41 @@ include Chef::Mixin::ShellOut
 
 module MacOS
   class SecurityCommand
+    attr_reader :cert
+    attr_reader :keychain
+    attr_reader :security
+
+    def initialize(cert, keychain)
+      @cert = cert
+      @keychain = keychain
+      @security = '/usr/bin/security'
+    end
+
+    def unlock_keychain(kc_passwd)
+      [@security, 'unlock-keychain', '-p', kc_passwd]
+    end
+
+    def add_certificates
+      if @keychain == ''
+        [@security, 'add-certificates', @cert]
+      else
+        [@security, 'add-certificates', @cert, '-k', @keychain]
+      end
+    end
+
+    def import(cert_passwd)
+      [@security, 'import', @cert, '-P', cert_passwd, @keychain]
+    end
+
+    def install_certificate(cert_passwd)
+      if ::File.extname(@cert) == '.p12'
+        import(cert_passwd)
+      elsif ::File.extname(@cert) == '.cer'
+        add_certificates
+      else
+        Chef::Exception.fatal('invalid cert file')
+      end
+    end
   end
 end
 
