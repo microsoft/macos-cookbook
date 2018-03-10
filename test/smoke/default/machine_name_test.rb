263beafdc44a814d0d08399ccfa089cb9231732c
Eric Hanko <v-erhank@microsoft.com>
2018-03-10 13:35:42 -0800

    update machine_name tests

--- test/smoke/default/machine_name_test.rb
+++ test/smoke/default/machine_name_test.rb
@@ -1,19 +1,17 @@
 macos_semantic_version = command('sw_vers -productVersion').stdout.strip
-friendly_pattern = Regexp.union("New#{macos_semantic_version}_Washing_Machine")
-hostname_pattern = Regexp.union("New#{os[:release].tr('.', '')}WashingMachine.body-of-swirling-water.com")
 
 control 'machine-name' do
   desc "machine name is set to the format \"New#{macos_semantic_version}_Washing_Machine\""
 
   describe command('scutil --get LocalHostName') do
-    its('stdout') { should match hostname_pattern }
+    its('stdout') { should match Regexp.union("New#{macos_semantic_version.tr('.', '')}WashingMachine") }
   end
 
   describe command('scutil --get HostName') do
-    its('stdout') { should match hostname_pattern }
+    its('stdout') { should match Regexp.union("New#{macos_semantic_version.tr('.', '')}WashingMachine.body-of-swirling-water.com") }
   end
 
   describe command('scutil --get ComputerName') do
-    its('stdout') { should match friendly_pattern }
+    its('stdout') { should match Regexp.union("New#{macos_semantic_version}_Washing_Machine") }
   end
 end
