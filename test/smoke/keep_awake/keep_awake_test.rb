0581d648678b4415192de7abb2ef34f432b37710
Eric Hanko <v-erhank@microsoft.com>
2018-03-12 16:46:41 -0700

    Minor modifications to keep awake
    The only significant change is to make the test suite match
    the default attribute of not disabling disk sleep. I also removed some
    tests that are not relevant to any modifications we're making to the
    power settings.
    
    It should also be noted that the plist we're checking for powernap is
    different for El Capitan, so I went ahead and removed that test for now
    (it was skipped previously)
    
    Signed-off-by: Eric Hanko <v-erhank@microsoft.com>

--- test/smoke/keep_awake/keep_awake_test.rb
+++ test/smoke/keep_awake/keep_awake_test.rb
@@ -1,11 +1,15 @@
 control 'power' do
-  desc 'Display, machine and hard disk never sleep, the machine automatically restarts
-  after a freeze or power outage, and Power Nap'
+  desc 'Computer sleep and display sleep are disabled, the machine automatically restarts
+  after a freeze or power outage, and Power Nap is disabled'
 
-  %w(sleep computersleep displaysleep harddisksleep).each do |setting|
+  %w(sleep computersleep displaysleep).each do |setting|
     describe command("systemsetup -get#{setting}") do
       its('stdout') { should match('Never') }
     end
+  end
+
+  describe command('systemsetup -getharddisksleep') do
+    its('stdout') { should match('Hard Disk Sleep: after 10 minutes') }
   end
 
   %w(restartfreeze remoteappleevents).each do |setting|
@@ -20,16 +24,8 @@ control 'power' do
 
   describe command('pmset -g') do
     its('stdout') { should match(/sleep\s*0/) }
-    its('stdout') { should match(/hibernatemode\s*0$/) }
-    its('stdout') { should match(/ttyskeepawake\s*1/) }
-    its('stdout') { should match(%r{hibernatefile\s*\/var\/vm\/sleepimage}) }
-    its('stdout') { should match(/disksleep\s*0/) }
+    its('stdout') { should match(/disksleep\s*10/) }
     its('stdout') { should match(/displaysleep\s*0/) }
-    its('stdout') { should match(/ttyskeepawake\s*1/) }
-  end
-
-  describe command("/usr/libexec/PlistBuddy -c 'Print :DarkWakeBackgroundTasks' '/Library/Preferences/com.apple.PowerManagement.plist'"), :skip do
-    its('stdout') { should match('false') }
   end
 end
 
