850b4806d5ab6f7fa4b40ec138bae3d0d258b8d9
Eric Hanko <v-erhank@microsoft.com>
2018-03-13 23:09:34 -0700

    add one second after each systemsetup call

--- resources/system_preference.rb
+++ resources/system_preference.rb
@@ -20,6 +20,11 @@ action :set do
     converge_by "set #{new_resource.preference} to #{new_resource.setting}" do
       set_pref = ['-set', new_resource.preference.to_s].join('')
       execute ['/usr/sbin/systemsetup', set_pref, new_resource.setting]
+      ruby_block 'sleep one second' do
+        block do
+          sleep 1
+        end
+      end
     end
   end
 end
