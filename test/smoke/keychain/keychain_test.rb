feddbc649518177b60fa8ac94c3a3090e83c8931
Mark John Merin <v-mamer@microsoft.com>
2018-03-16 13:43:30 -0700

    Add basic integration tests
    - macOS 10.11.6 stores the keychain differently than 10.12 or 10.13.

--- test/smoke/keychain/keychain_test.rb
+++ test/smoke/keychain/keychain_test.rb
@@ -1,9 +1,12 @@
-control 'keychian creation' do
-  describe file('/Users/vagrant/Library/Keychains/test.keychain') do
-    it { should exist }
-  end
+control 'keychain creation' do
+  if platform_version?('10.11.6')
+    describe file('/Users/vagrant/Library/Keychains/test.keychain') do
+      it { should exist }
+    end
 
-  # describe command('/usr/bin/security find-certificate /Users/vagrant/Library/Keychains/login.keychain') do
-  #   its('stdout') { should include 'Test' }
-  # end
+  else
+    describe file('/Users/vagrant/Library/Keychains/test.keychain-db') do
+      it { should exist }
+    end
+  end
 end
