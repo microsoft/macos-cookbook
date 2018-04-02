d6a277da50acbd6468f37d11784d726c49df15d1
Jacob Zaval <jazava@microsoft.com>
2018-04-02 15:11:04 -0700

    add Xcode beta suite

--- test/cookbooks/macos_test/recipes/xcode_beta.rb
+++ test/cookbooks/macos_test/recipes/xcode_beta.rb
@@ -3,11 +3,5 @@ if node['platform_version'].match? Regexp.union ['10.12', '10.13']
     command ['spctl', '--master-disable']
   end
 
-  include_recipe 'macos::xcode'
-
-elsif node['platform_version'].match? Regexp.union '10.11'
-
-  xcode '8.2.1' do
-    ios_simulators %w(10 9)
-  end
+  xcode '9.4'
 end
