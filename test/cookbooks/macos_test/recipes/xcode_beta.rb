71988409d52430e64ffa6f3f5a83e587e4cb7219
Jacob Zaval <jazava@microsoft.com>
2018-04-03 11:26:39 -0700

    update test cookbook since 9.3 can't install on Sierra

--- test/cookbooks/macos_test/recipes/xcode_beta.rb
+++ test/cookbooks/macos_test/recipes/xcode_beta.rb
@@ -1,7 +1,5 @@
-if node['platform_version'].match? Regexp.union ['10.12', '10.13']
-  execute 'Disable Gatekeeper' do
-    command ['spctl', '--master-disable']
-  end
-
-  xcode '9.4'
+execute 'Disable Gatekeeper' do
+  command ['spctl', '--master-disable']
 end
+
+xcode '9.4'
