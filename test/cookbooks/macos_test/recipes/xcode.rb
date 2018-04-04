0094f91bb41df1c840151b5d540e111a85934ec6
Jacob Zaval <jazava@microsoft.com>
2018-04-04 14:16:05 -0700

    apparently Xcode 8.0 doesn't have any available simulators anymore

--- test/cookbooks/macos_test/recipes/xcode.rb
+++ test/cookbooks/macos_test/recipes/xcode.rb
@@ -6,12 +6,12 @@ if node['platform_version'].match? Regexp.union '10.13'
   include_recipe 'macos::xcode'
 
 elsif node['platform_version'].match? Regexp.union '10.12'
-  xcode '9.2' do
+  xcode '9.0' do
     ios_simulators %w(11 10)
   end
 
 elsif node['platform_version'].match? Regexp.union '10.11'
-  xcode '8.0' do
+  xcode '8.2.1' do
     ios_simulators %w(10 9)
   end
 end
