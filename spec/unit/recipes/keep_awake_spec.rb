280f8b077ddad60d6391e3e67c7179c35266e330
Mark John Merin <33106688+mjmerin@users.noreply.github.com>
2018-04-23 10:27:28 -0700

    New Keep Awake Tests (#99)
    * Add test case for portable bare metal
    
    * rename systemsetup to system
    
    * Fix spec_helper to use system
    
    * cookstyle changes

--- spec/unit/recipes/keep_awake_spec.rb
+++ spec/unit/recipes/keep_awake_spec.rb
@@ -10,6 +10,12 @@ shared_context 'when running on bare metal macmini' do
       stub_command('which git').and_return('/usr/bin/git')
     end
 
+    it 'returns bare metal desktop' do
+      ff = System::FormFactor.new()
+      expect(ff.desktop?).to be true
+      expect(ff.portable?).to be false
+    end
+
     it 'returns false' do
       env = System::Environment.new('host')
       expect(env.vm?).to be false
@@ -36,6 +42,41 @@ shared_context 'when running on bare metal macmini' do
   end
 end
 
+shared_context 'when running on bare metal macbook' do
+  shared_examples 'setting portable metal-specific power preferences' do
+    before(:each) do
+      chef_run.node.normal['hardware']['machine_model'] = 'Macbook'
+      chef_run.node.normal['virtualization']['systems'] = { 'vbox' => 'host', 'parallels' => 'host' }
+      stub_command('which git').and_return('/usr/bin/git')
+    end
+
+    it 'returns bare metal portable' do
+      ff = System::FormFactor.new()
+      expect(ff.portable?).to be true
+      expect(ff.desktop?).to be false
+    end
+
+    it 'returns false' do
+      env = System::Environment.new()
+      expect(env.vm?).to be false
+    end
+
+    it 'sets wake on lan' do
+      chef_run.converge(described_recipe)
+      expect(chef_run).to set_system_preference('wake the computer when accessed using a network connection')
+    end
+
+    it 'sets restart after a power failure' do
+      chef_run.converge(described_recipe)
+      expect(chef_run).to set_system_preference('restart after a power failure')
+    end
+
+    it 'converges successfully on bare metal' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
+end
+
 shared_context 'running in a parallels virtual machine' do
   before(:each) do
     chef_run.node.normal['virtualization']['systems'] = { 'parallels' => 'guest' }
@@ -44,6 +85,12 @@ shared_context 'running in a parallels virtual machine' do
   end
 
   shared_examples 'not setting metal-specific power prefs' do
+    it 'returns not on bare metal' do
+      ff = System::FormFactor.new()
+      expect(ff.desktop?).to be false
+      expect(ff.portable?).to be false
+    end
+
     it 'confirms we are in a vm' do
       env = System::Environment.new()
       expect(env.vm?).to be true
@@ -78,6 +125,12 @@ shared_context 'running in an undetermined virtualization system' do
   end
 
   shared_examples 'not setting metal-specific power prefs' do
+    it 'returns not on bare metal' do
+      ff = System::FormFactor.new()
+      expect(ff.desktop?).to be false
+      expect(ff.portable?).to be false
+    end
+
     it 'assumes we are in a vm' do
       env = System::Environment.new('undetermined')
       expect(env.vm?).to be true
@@ -120,5 +173,10 @@ describe 'macos::keep_awake' do
   describe 'keep_awake on bare metal' do
     include_context 'when running on bare metal macmini'
     it_behaves_like 'setting metal-specific power preferences'
+  end
+
+  describe 'keep_awake on portable bare metal' do
+    include_context 'when running on bare metal macbook'
+    it_behaves_like 'setting portable metal-specific power preferences'
   end
 end
