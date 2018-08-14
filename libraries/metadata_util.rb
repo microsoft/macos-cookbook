2d18a9f328b813e8793de3d05d4fbbc83721ba3d
Mark John Merin <v-mamer@microsoft.com>
2018-08-14 11:39:41 -0700

    add additional functions that check that the server is disabled and a toggle function

--- libraries/metadata_util.rb
+++ libraries/metadata_util.rb
@@ -7,7 +7,8 @@ module MacOS
     def initialize(volume)
       mdutil_possible_states = { 'Indexing enabled.' => ['on', ''],
                                  'Indexing disabled.' => ['off', ''],
-                                 'Indexing and searching disabled.' => ['off', '-d'] }
+                                 'Indexing and searching disabled.' => ['off', '-d'],
+                                 'Spotlight server is disabled.' => %w(dis dis) }
 
       @status_flags = mdutil_possible_states[volume_current_state(volume)]
                       .insert(1, volume)
@@ -17,8 +18,18 @@ module MacOS
       shell_out('/usr/bin/mdutil', '-s', volume).stdout
     end
 
+    def server_disabled?(volume)
+      status_flags == ['dis', volume, 'dis']
+    end
+
+    def toggle_spotlight_server(flag)
+      action = flag ? 'load' : 'unload'
+      ['sudo', 'launchctl', action, '-w', '/System/Library/LaunchDaemons/com.apple.metadata.mds.plist']
+    end
+
     def volume_current_state(volume)
-      mdutil_output(volume).split(':')[1].strip
+      output = mdutil_output(volume)
+      (output.include? ':') ? mdutil_output(volume).split(':')[1].strip : output.strip
     end
   end
 end
