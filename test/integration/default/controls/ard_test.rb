8d42d526ad5b61fc2f20aff2e32eea7f4806bed3
Andre Shields <v-anshie@microsoft.com>
2018-09-20 16:24:38 -0700

    update syntax in ard_test.rb, added mojave test suites, removed test suites for el capitan

--- test/integration/default/controls/ard_test.rb
+++ test/integration/default/controls/ard_test.rb
@@ -1,16 +1,16 @@
-title `ard`
+title 'ard'
 
-dscl = /usr/bin/dscl
+dscl = '/usr/bin/dscl'
 
-control `naprivs` do
-  title `naprivs value for remote management`
-  desc `Verify that naprivs in the users.plist has the value -1073741569`
+control 'naprivs' do
+  title 'naprivs value for remote management'
+  desc 'Verify that naprivs in the users.plist has the value -1073741569'
 
-  describe command (`#{dscl} . list /Users naprivs`) do
+  describe command ('#{dscl} . list /Users naprivs') do
       its('stdout') { should_match (/vagrant -1073741569/) }
   end
 
-  describe command (`sudo -E PlistBuddy -c 'Print naprivs:0' /var/db/dslocal/nodes/Default/users/vagrant.plist`) do
+  describe command ("sudo -E PlistBuddy -c 'Print naprivs:0' /var/db/dslocal/nodes/Default/users/vagrant.plist") do
     its('stdout') { should_match (/-1073741569/) }
   end
 end
