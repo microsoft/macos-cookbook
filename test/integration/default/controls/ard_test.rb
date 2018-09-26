d2475557c8b7def2b7ea1cb0c96098a924c02a31
Jacob Zaval <jazava@microsoft.com>
2018-09-26 14:05:40 -0700

    update inspec tests

--- test/integration/default/controls/ard_test.rb
+++ test/integration/default/controls/ard_test.rb
@@ -5,6 +5,10 @@ control 'remote-control' do
   desc 'verify that naprivs has the bitmask value -1073741569'
 
   describe command('/usr/bin/dscl . list /Users naprivs') do
-    its('stdout') { should match 'vagrant   -1073741569' }
+    its('stdout') { should match 'vagrant   -2147483648' }
+  end
+
+  describe command('defaults read /Library/Preferences/com.apple.RemoteManagement ARD_AllLocalUsersPrivs') do
+    its('stdout') { should match '1073742079' }
   end
 end
