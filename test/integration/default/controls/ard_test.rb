8557c073bc840671c94363e4efc804323dcb133b
Mark John Merin <v-mamer@microsoft.com>
2018-09-27 13:19:03 -0700

    remove unneeded tests

--- test/integration/default/controls/ard_test.rb
+++ test/integration/default/controls/ard_test.rb
@@ -1,6 +1,6 @@
 title 'remote access'
 
-user_has_access  = 1 << 31
+# user_has_access  = 1 << 31
 text_messages    = 1 << 0
 control_observe  = 1 << 1
 send_files       = 1 << 2
@@ -9,6 +9,7 @@ generate_reports = 1 << 4
 open_quit_apps   = 1 << 5
 change_settings  = 1 << 6
 restart_shutdown = 1 << 7
+# observe_only     = 1 << 8
 show_observe     = 1 << 30
 
 all_privileges = text_messages | control_observe | send_files |
@@ -17,17 +18,10 @@ all_privileges = text_messages | control_observe | send_files |
 
 control 'remote-control' do
   title 'naprivs value represents remote control for all users'
-  desc "verify that naprivs has the bitmask value #{all_privileges}"
-
-  describe command('/usr/bin/dscl . list /Users naprivs') do
-    its('stdout') { should match /#{user_has_access}/ }
-  end
-
-  describe command('defaults read /Library/Preferences/com.apple.RemoteManagement ARD_AllLocalUsersPrivs') do
-    its('stdout') { should cmp all_privileges }
-  end
+  desc 'Verify that naprivs has the magic value to indicate all privileges'
 
   describe command('/usr/libexec/PlistBuddy -c Print /Library/Preferences/com.apple.RemoteManagement.plist') do
     its('stdout') { should match 'ARD_AllLocalUsers = true' }
+    its('stdout') { should match /#{all_privileges}/ }
   end
 end
