4bcc34d839469d728011dbd512282b947f9c0d34
Jacob Zaval <jazava@microsoft.com>
2018-10-01 10:11:17 -0700

    update spec with output from Mojave

--- spec/unit/libraries/command_line_tools_spec.rb
+++ spec/unit/libraries/command_line_tools_spec.rb
@@ -8,12 +8,18 @@ describe MacOS::CommandLineTools do
       allow(FileUtils).to receive(:chown).and_return(true)
       allow_any_instance_of(MacOS::CommandLineTools).to receive(:softwareupdate_list)
         .and_return(["Software Update Tool\n",
-                     "Finding available software\n",
+                     "\n", "Finding available software\n",
                      "Software Update found the following new or updated software:\n",
                      "   * Command Line Tools (macOS El Capitan version 10.11) for Xcode-8.2\n",
                      "\tCommand Line Tools (macOS El Capitan version 10.11) for Xcode (8.2), 150374K [recommended]\n",
-                     "   * Command Line Tools (macOS High Sierra version 10.13) for Xcode-9.2\n",
-                     "\tCommand Line Tools (macOS High Sierra version 10.13) for Xcode (9.2), 177376K [recommended]\n"]
+                     "   * Command Line Tools (macOS High Sierra version 10.13) for Xcode-10.0\n",
+                     "\tCommand Line Tools (macOS High Sierra version 10.13) for Xcode (10.0), 190520K [recommended]\n",
+                     "   * Command Line Tools (macOS Mojave version 10.14) for Xcode-10.0\n",
+                     "\tCommand Line Tools (macOS Mojave version 10.14) for Xcode (10.0), 187321K [recommended]\n",
+                     "   * Command Line Tools (macOS High Sierra version 10.13) for Xcode-9.3\n",
+                     "\tCommand Line Tools (macOS High Sierra version 10.13) for Xcode (9.3), 187312K [recommended]\n",
+                     "   * Command Line Tools (macOS High Sierra version 10.13) for Xcode-9.4\n",
+                     "\tCommand Line Tools (macOS High Sierra version 10.13) for Xcode (9.4), 187380K [recommended]\n"]
                    )
     end
     it 'returns the latest recommended Command Line Tools product' do
