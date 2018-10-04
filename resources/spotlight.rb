8105982649c4dff7ac6f66e6c2690e3aff581f48
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-10-04 11:27:56 -0700

    Update properties to align more with `mdutil` logic

--- resources/spotlight.rb
+++ resources/spotlight.rb
@@ -1,38 +1,38 @@
 resource_name :spotlight
 default_action :set
 
-property :volume, String, name_property: true
-property :indexed, [true, false], default: true
+property :directory, String, name_property: true
+property :indexing, ['enabled', 'disabled'], default: 'enabled'
 property :searchable, [true, false], default: true
 
 action_class do
   def state
-    new_resource.indexed ? 'on' : 'off'
+    new_resource.indexing == 'enabled' ? 'on' : 'off'
   end
 
   def search
     new_resource.searchable ? '' : '-d'
   end
 
-  def volume_path(volume)
-    volume == '/' ? volume : ::File.join('/Volumes', volume)
-  end
-
-  def target_volume
-    volume_path(new_resource.volume)
-  end
-
   def mdutil
     ['/usr/bin/mdutil']
   end
 
-  def mdutil_output(volume)
-    shell_out('/usr/bin/mdutil', '-s', volume_path(volume)).stdout
+  def mdutil_status
+    shell_out(mdutil, '-s', new_resource.directory).stdout
+  end
+
+  def index_status
+    mdutil_status.split(':')[1]
   end
 
   def desired_spotlight_state
-    [state, target_volume, search]
+    [state, new_resource.directory, search]
   end
+end
+
+load_current_value do
+  indexing shell_out('/usr/bin/mdutil', '-s', directory).stdout.split(':')[1].split(' ')[1]
 end
 
 action :set do
@@ -42,8 +42,9 @@ action :set do
     action [:enable, :start]
   end
 
-  execute "turn Spotlight indexing #{state} for #{target_volume}" do
-    command mdutil + desired_spotlight_state.insert(0, '-i')
-    not_if { mdutil_output(target_volume) == desired_spotlight_state }
+  converge_if_changed :indexing do
+    execute "Spotlight indexing is #{state}" do
+      command mdutil + desired_spotlight_state.insert(0, '-i')
+    end
   end
 end
