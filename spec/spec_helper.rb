6b0d7be1942e7c701ba794b8db2be62a56878b74
Eammon Hanlon <ehanlon@users.noreply.github.com>
2018-10-09 14:06:46 -0700

    Remove machine_name resource in favor of hostname resource in Chef14 (#145)
    * update syntax in ard_test.rb, added mojave test suites, removed test suites for el capitan
    
    * update inspec tests
    
    * remove live_stream option
    
    * Remove machine name custom resource and related tests
    
    * Add deprecation notice for the machine_name resource (#146)
    
    * Add spotlight spec test
    
    * Add back el-cap chef14 platform in .kitchen.yml
    
    * Release 3.0 will not support Chef 13 if this change is implemented

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 
 require_relative '../libraries/macos_user'
-require_relative '../libraries/machine_name'
 require_relative '../libraries/metadata_util'
 require_relative '../libraries/plist'
 require_relative '../libraries/system'
--- test/cookbooks/macos_test/recipes/machine_name.rb
+++
@@ -1,8 +0,0 @@
-washing_machine_name = 'New' + node['platform_version'] + '_Washing_Machine'
-
-machine_name 'set computer/hostname' do
-  hostname washing_machine_name
-  computer_name washing_machine_name
-  local_hostname washing_machine_name
-  dns_domain 'body-of-swirling-water.com'
-end
