a4431c2f921f6d7773aa5b07b87ba9e9cdb589b9
Jacob Zaval <jazava@microsoft.com>
2018-11-06 14:30:00 -0800

    Update libraries/command_line_tools.rb
    Co-Authored-By: mjmerin <33106688+mjmerin@users.noreply.github.com>

--- libraries/command_line_tools.rb
+++ libraries/command_line_tools.rb
@@ -10,7 +10,7 @@ module MacOS
       FileUtils.chown 'root', 'wheel', install_sentinel
 
       @version = if available.empty?
-                   'Unavailable from Software Update Catalog'
+                   'No Command Lines Tools available from Software Update Catalog!'
                  elsif platform_specific.empty?
                    'Absent from Software Update Catalog'
                  else
