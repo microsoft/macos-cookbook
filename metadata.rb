d0751bca111e3f6989a0968a8b8c3a539b498098
Eric Hanko <eric.hanko1@gmail.com>
2018-11-20 12:04:31 -0800

    Release/2.8 (#167)
    * Bump cookbook version to 2.8
    
    *  Add dependency on chef-sugar cookbook to provide platform helper methods (#162)
    
    * Use platform methods from chef-sugar for guards in Xcode recipe
    
    * Depend on chef-sugar cookbook for some added sugar
    
    * Add El Capitan and an exception for an unsupported version
    
    * Add version pinning for chef-sugar
      Co-Authored-By: ehanlon <eammon.hanlon@microsoft.com>
    
    * Remove trailing whitespace
    
    * update build badge
    
    * add master parameter
    
    * Fix for Issue #163 (#164)
    
    * add additional check when grabbing the version
    
    * add additional test with an incomplete list to see if library changes work
    
    * Update libraries/command_line_tools.rb
    
    Co-Authored-By: mjmerin <33106688+mjmerin@users.noreply.github.com>
    
    * Update libraries/command_line_tools.rb
    
    Co-Authored-By: mjmerin <33106688+mjmerin@users.noreply.github.com>
    
    * Update spec/unit/libraries/command_line_tools_spec.rb
    
    Co-Authored-By: mjmerin <33106688+mjmerin@users.noreply.github.com>
    
    * fix unterminated string
    
    * bump base box to the new 10.14.1
    
    * added changlelog entry for release/2.8
    
    * Update CHANGELOG.md
    
    Co-Authored-By: v-anshie <v-anshie@microsoft.com>
    
    * Update CHANGELOG.md
    
    Co-Authored-By: v-anshie <v-anshie@microsoft.com>
    
    * Update CHANGELOG.md
    
    Co-Authored-By: v-anshie <v-anshie@microsoft.com>

--- metadata.rb
+++ metadata.rb
@@ -5,9 +5,11 @@ license 'MIT'
 description 'Resources for configuring and provisioning macOS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 chef_version '>= 13.0' if respond_to?(:chef_version)
-version '2.7.0'
+version '2.8.0'
 
 source_url 'https://github.com/Microsoft/macos-cookbook'
 issues_url 'https://github.com/Microsoft/macos-cookbook/issues'
 
 supports 'mac_os_x'
+
+depends 'chef-sugar', '~> 4.1'
