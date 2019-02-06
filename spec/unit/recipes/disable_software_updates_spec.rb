484b465d653a4e1277d6c173269cb201c4b6c3d9
Eammon Hanlon <eahanl@microsoft.com>
2019-02-06 15:15:35 -0800

    Remove default recipe and fix ChefSpec test for disable_software_updates recipe (#186)
    * Delete default recipe and related spec
    
    * Fix failing ChefSpec test for disable_software_updates recipe

--- spec/unit/recipes/disable_software_updates_spec.rb
+++ spec/unit/recipes/disable_software_updates_spec.rb
@@ -2,11 +2,15 @@ require 'spec_helper'
 
 describe 'macos::disable_software_updates' do
   context 'When all attributes are default, on macOS High Sierra 10.13' do
-    let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
-
-    it 'converges successfully' do
+    before do
       allow_any_instance_of(MacOS::SoftwareUpdates).to receive(:automatic_check_disabled?)
         .and_return(false)
+      stubs_for_resource('execute[disable software updates using commandline utility]') do |resource|
+        allow(resource).to receive_shell_out('/usr/sbin/softwareupdate', '--schedule')
+      end
+    end
+
+    it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
   end
