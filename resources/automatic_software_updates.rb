f505386e9f09af0b5785f356086cc5ed435a9ee4
Andre Shields <v-anshie@microsoft.com>
2019-02-12 15:44:02 -0800

    add new automatic_software_updates resource

---
+++ resources/automatic_software_updates.rb
@@ -0,0 +1,46 @@
+resource_name :automatic_software_updates
+
+property :check, [TrueClass, FalseClass]
+
+property :download, [TrueClass, FalseClass]
+
+property :os, [TrueClass, FalseClass]
+
+property :app_store, [TrueClass, FalseClass]
+
+property :critical, [TrueClass, FalseClass]
+
+action :set do
+  plist '/Library/Preferences/com.apple.SoftwareUpdate.plist' do
+    entry 'AutomaticCheckEnabled'
+    value true
+  end
+end
+
+action :set do
+  plist '/Library/Preferences/com.apple.SoftwareUpdate.plist' do
+    entry 'AutomaticDownload'
+    value true
+  end
+end
+
+action :set do
+  plist '/Library/Preferences/com.apple.SoftwareUpdate.plist' do
+    entry 'AutomaticallyInstallMacOSUpdates'
+    value true
+  end
+end
+
+action :set do
+  plist '/Library/Preferences/com.apple.SoftwareUpdate.plist' do
+    entry 'CriticalUpdateInstall'
+    value true
+  end
+end
+
+# action :set do
+  # plist '/Library/Preferences/com.apple.commerce.plist' do
+    # entry 'AutoUpdate'
+    # value true
+  # end
+# end
