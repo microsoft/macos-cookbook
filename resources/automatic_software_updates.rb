49605550fe39e7e962884812d90f4715dc96e76e
Jacob Zaval <jazava@microsoft.com>
2019-02-14 17:03:07 -0800

    create app store plist variable

--- resources/automatic_software_updates.rb
+++ resources/automatic_software_updates.rb
@@ -11,35 +11,36 @@ property :install_app_store, [TrueClass, FalseClass]
 property :install_critical, [TrueClass, FalseClass]
 
 software_update_plist = '/Library/Preferences/com.apple.SoftwareUpdate.plist'
+app_store_plist = '/Library/Preferences/com.apple.commerce.plist'
 
 action :set do
   plist 'entry for AutomaticCheckEnabled' do
-    path software_update_plist
     entry 'AutomaticCheckEnabled'
     value new_resource.check
+    path software_update_plist
   end
 
   plist 'entry for AutomaticDownload' do
-    path software_update_plist
     entry 'AutomaticDownload'
     value new_resource.download
+    path software_update_plist
   end
 
   plist 'entry for CriticalUpdateInstall' do
-    path software_update_plist
     entry 'CriticalUpdateInstall'
     value new_resource.install_critical
+    path software_update_plist
   end
 
   plist 'entry for AutomaticallyInstallMacOSUpdates' do
-    path software_update_plist
     entry 'AutomaticallyInstallMacOSUpdates'
     value new_resource.install_os
+    path software_update_plist
   end
 
   plist 'entry for AutoUpdate' do
-    path '/Library/Preferences/com.apple.commerce.plist'
     entry 'AutoUpdate'
     value new_resource.install_app_store
+    path app_store_plist
   end
 end
