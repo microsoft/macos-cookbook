79fd684533cb4a6262b3ae1393f50233c3aba038
Jacob Zaval <jazava@microsoft.com>
2019-02-14 17:01:43 -0800

    lint and beautify

--- resources/automatic_software_updates.rb
+++ resources/automatic_software_updates.rb
@@ -10,30 +10,29 @@ property :install_app_store, [TrueClass, FalseClass]
 
 property :install_critical, [TrueClass, FalseClass]
 
-path_to_software_update_plist = "/Library/Preferences/com.apple.SoftwareUpdate.plist"
+software_update_plist = '/Library/Preferences/com.apple.SoftwareUpdate.plist'
 
 action :set do
-
   plist 'entry for AutomaticCheckEnabled' do
-    path path_to_software_update_plist
+    path software_update_plist
     entry 'AutomaticCheckEnabled'
     value new_resource.check
   end
 
   plist 'entry for AutomaticDownload' do
-    path path_to_software_update_plist
+    path software_update_plist
     entry 'AutomaticDownload'
     value new_resource.download
   end
 
   plist 'entry for CriticalUpdateInstall' do
-    path path_to_software_update_plist
+    path software_update_plist
     entry 'CriticalUpdateInstall'
     value new_resource.install_critical
   end
 
   plist 'entry for AutomaticallyInstallMacOSUpdates' do
-    path path_to_software_update_plist
+    path software_update_plist
     entry 'AutomaticallyInstallMacOSUpdates'
     value new_resource.install_os
   end
