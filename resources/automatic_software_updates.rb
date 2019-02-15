536b86015e166311b9ba3744efe244ccdaada7b4
v-anshie <v-anshie@microsoft.com>
2019-02-14 16:02:53 -0800

    spec tests with seperate it blocks, work in progress

--- resources/automatic_software_updates.rb
+++ resources/automatic_software_updates.rb
@@ -12,22 +12,31 @@ property :install_critical, [TrueClass, FalseClass]
 
 action :set do
   plist '/Library/Preferences/com.apple.SoftwareUpdate.plist' do
-    entry 'AutomaticCheckEnabled'
-    value new_resource.check
-  end
-  plist '/Library/Preferences/com.apple.SoftwareUpdate.plist' do
+    # path '/Library/Preferences/com.apple.SoftwareUpdate.plist'
     entry 'AutomaticDownload'
     value new_resource.download
   end
-  plist '/Library/Preferences/com.apple.SoftwareUpdate.plist' do
-    entry 'AutomaticallyInstallMacOSUpdates'
-    value new_resource.install_os
-  end
-  plist '/Library/Preferences/com.apple.SoftwareUpdate.plist' do
+
+  plist 'Library/Preferences/com.apple.SoftwareUpdate.plist' do
+    # path '/Library/Preferences/com.apple.SoftwareUpdate.plist'
     entry 'CriticalUpdateInstall'
     value new_resource.install_critical
   end
+
+  plist '/Library/Preferences/com.apple.SoftwareUpdate.plist' do
+    # path '/Library/Preferences/com.apple.SoftwareUpdate.plist'
+    entry 'AutomaticCheckEnabled'
+    value new_resource.check
+  end
+
+  plist 'Library/Preferences/com.apple.SoftwareUpdate.plist' do
+    # path '/Library/Preferences/com.apple.SoftwareUpdate.plist'
+    entry 'AutomaticallyInstallMacOSUpdates'
+    value new_resource.install_os
+  end
+
   plist '/Library/Preferences/com.apple.commerce.plist' do
+    # path '/Library/Preferences/com.apple.commerce.plist'
     entry 'AutoUpdate'
     value new_resource.install_app_store
   end
--- spec/unit/resources/automatic_software_updates_spec.rb
+++ spec/unit/resources/automatic_software_updates_spec.rb
@@ -1,38 +1,11 @@
 require 'spec_helper'
 
-describe 'automatic_software_updates' do
+describe 'automatic_software_updates_false' do
   step_into :automatic_software_updates
   platform 'mac_os_x'
 
   recipe do
-    automatic_software_updates 'enables automatic check and download' do
-      check true
-      download true
-      install_os true
-      install_app_store true
-      install_critical true
-    end
-  end
-
-  it {
-    is_expected.to set_plist('/Library/Preferences/com.apple.SoftwareUpdate.plist')
-      .with(entry: 'AutomaticCheckEnabled', value: true)
-      .with(entry: 'AutomaticDownload', value: true)
-      .with(entry: 'AutomaticallyInstallMacOSUpdates', value: true)
-      .with(entry: 'CriticalUpdateInstall', value: true)
-    is_expected.to set_plist('/Library/Preferences/com.apple.commerce.plist')
-      .with(entry: 'AutoUpdate', value: true)
-  }
-end
-
-require 'spec_helper'
-
-describe 'automatic_software_updates' do
-  step_into :automatic_software_updates
-  platform 'mac_os_x'
-
-  recipe do
-    automatic_software_updates 'enables automatic check and download' do
+    automatic_software_updates 'disables automatic check and download' do
       check false
       download false
       install_os false
@@ -40,14 +13,9 @@ describe 'automatic_software_updates' do
       install_critical false
     end
   end
-
-  it {
-    is_expected.to set_plist('/Library/Preferences/com.apple.SoftwareUpdate.plist')
-      .with(entry: 'AutomaticCheckEnabled', value: false)
-      .with(entry: 'AutomaticDownload', value: false)
-      .with(entry: 'AutomaticallyInstallMacOSUpdates', value: false)
-      .with(entry: 'CriticalUpdateInstall', value: false)
-    is_expected.to set_plist('/Library/Preferences/com.apple.commerce.plist')
-      .with(entry: 'AutoUpdate', value: false)
-  }
+  it { is_expected.to set_plist('/Library/Preferences/com.apple.SoftwareUpdate.plist').with(entry: 'AutomaticCheckEnabled', value: false).and set_plist('/Library/Preferences/com.apple.SoftwareUpdate.plist').with(entry: 'AutomaticDownload', value: false) }
+  # it { is_expected.to set_plist('/Library/Preferences/com.apple.SoftwareUpdate.plist').with(entry: 'AutomaticDownload', value: false) }
+  # it { is_expected.to set_plist('/Library/Preferences/com.apple.SoftwareUpdate.plist').with(entry: 'AutomaticallyInstallMacOSUpdates', value: false) }
+  # it { is_expected.to set_plist('/Library/Preferences/com.apple.SoftwareUpdate.plist').with(entry: 'CriticalUpdateInstall', value: false) }
+  # it { is_expected.to set_plist('/Library/Preferences/com.apple.commerce.plist').with(entry: 'AutoUpdate', value: false) }
 end
