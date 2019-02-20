769d66a45f26b31f05a3d1bb6954b35efc1f68c6
v-anshie <v-anshie@microsoft.com>
2019-02-20 15:37:10 -0800

    add error handling to resource to check against bad configs

--- resources/automatic_software_updates.rb
+++ resources/automatic_software_updates.rb
@@ -14,6 +14,18 @@ software_update_plist = '/Library/Preferences/com.apple.SoftwareUpdate.plist'
 app_store_plist = '/Library/Preferences/com.apple.commerce.plist'
 
 action :set do
+  unless new_resource.check
+    if new_resource.download
+      raise "No other properties of this resource can be true if 'check' is false"
+    end
+  end
+
+  unless new_resource.download
+    if new_resource.install_os || new_resource.install_app_store
+      raise "OS or App Store updates cannot be enabled if 'download' is false"
+    end
+  end
+
   plist 'entry for AutomaticCheckEnabled' do
     entry 'AutomaticCheckEnabled'
     value new_resource.check
