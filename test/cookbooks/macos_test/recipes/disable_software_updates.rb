576bf22f742e6657feea0e260dc0a5ffd7ab1ada
v-anshie <v-anshie@microsoft.com>
2019-02-20 16:16:20 -0800

    add disable_software_updates.rb

---
+++ test/cookbooks/macos_test/recipes/disable_software_updates.rb
@@ -0,0 +1,7 @@
+automatic_software_updates 'disabled automatic check and download' do
+    check false
+    download false
+    install_os false
+    install_app_store false
+    install_critical false
+end
