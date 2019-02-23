a8f109c9d0b3f764061504e4f568d6623316bf89
Jacob Zaval <jazava@microsoft.com>
2019-02-22 16:02:36 -0800

    update spec to use shared examples and contexts

--- spec/unit/resources/remote_management_spec.rb
+++ spec/unit/resources/remote_management_spec.rb
@@ -1,86 +1,102 @@
 require 'spec_helper'
 
-describe 'remote_management' do
+shared_context 'with remote management enabled' do
   step_into :remote_management
   platform 'mac_os_x'
 
-  context 'with remote management not already enabled' do
-    before do
-      allow(File).to receive(:exist?).and_call_original
-      allow(File).to receive(:exist?)
-        .with('/Library/Application Support/Apple/Remote Desktop/RemoteManagement.launchd')
-        .and_return(false)
-    end
+  before do
+    allow(File).to receive(:exist?).and_call_original
+    allow(File).to receive(:exist?)
+      .with('/Library/Application Support/Apple/Remote Desktop/RemoteManagement.launchd')
+      .and_return(true)
+    allow(RemoteManagement).to receive(:plist)
+      .and_return 'Dict { ARD_AllLocalUsersPrivs = 1073742079
+                          ARD_AllLocalUsers = true }'
+  end
+end
 
-    recipe do
-      remote_management 'enabled' do
-        action :enable
-      end
-    end
+shared_context 'with remote management disabled' do
+  step_into :remote_management
+  platform 'mac_os_x'
 
-    it { is_expected.to run_execute('/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate') }
-    it { is_expected.to run_execute('/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -access -on -privs -all') }
+  before do
+    allow(File).to receive(:exist?).and_call_original
+    allow(File).to receive(:exist?)
+      .with('/Library/Application Support/Apple/Remote Desktop/RemoteManagement.launchd')
+      .and_return(false)
+    allow(RemoteManagement).to receive(:plist)
+      .and_return ''
+  end
+end
+
+shared_examples 'kickstart activating and configuring the ARD agent' do
+  it { is_expected.to run_execute('/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate') }
+  it { is_expected.to run_execute('/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -access -on -privs -all') }
+end
+
+shared_examples 'kickstart deactivating and stopping the ARD agent' do
+  it { is_expected.to run_execute('/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate') }
+  it { is_expected.to run_execute('/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -stop') }
+end
+
+shared_examples 'kickstart not activating or configuring the ARD agent' do
+  it { is_expected.to_not run_execute('/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate') }
+  it { is_expected.to_not run_execute('/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -access -on -privs -all') }
+end
+
+shared_examples 'kickstart not deactivating or stopping the ARD agent' do
+  it { is_expected.to_not run_execute('/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate') }
+  it { is_expected.to_not run_execute('/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -stop') }
+end
+
+describe 'enabling when already disabled' do
+  include_context 'with remote management disabled'
+
+  recipe do
+    remote_management 'enabled' do
+      action :enable
+    end
   end
 
-  context 'with remote management not already enabled' do
-    before do
-      allow(File).to receive(:exist?).and_call_original
-      allow(File).to receive(:exist?)
-        .with('/Library/Application Support/Apple/Remote Desktop/RemoteManagement.launchd')
-        .and_return(false)
-    end
+  it_behaves_like 'kickstart activating and configuring the ARD agent'
+  it_behaves_like 'kickstart not deactivating or stopping the ARD agent'
+end
 
-    recipe do
-      remote_management 'disabled' do
-        action :disable
-      end
-    end
+describe 'enabling when already enabled' do
+  include_context 'with remote management enabled'
 
-    it { is_expected.to_not run_execute('/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate') }
-    it { is_expected.to_not run_execute('/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -stop') }
+  recipe do
+    remote_management 'enabled' do
+      action :enable
+    end
   end
 
-  context 'with remote management already enabled and configured' do
-    before do
-      allow(File).to receive(:exist?).and_call_original
-      allow(File).to receive(:exist?)
-        .with('/Library/Application Support/Apple/Remote Desktop/RemoteManagement.launchd')
-        .and_return(true)
-      allow(RemoteManagement).to receive(:plist)
-        .and_return 'Dict { ARD_AllLocalUsersPrivs = 1073742079
-                            allowInsecureDH = true
-                            ARD_AllLocalUsers = true }'
-    end
+  it_behaves_like 'kickstart not activating or configuring the ARD agent'
+  it_behaves_like 'kickstart not deactivating or stopping the ARD agent'
+end
 
-    recipe do
-      remote_management 'enabled' do
-        action :enable
-      end
-    end
+describe 'disabling when already disabled' do
+  include_context 'with remote management disabled'
 
-    it { is_expected.to_not run_execute('/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate') }
-    it { is_expected.to_not run_execute('/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -access -on -privs -all') }
+  recipe do
+    remote_management 'disabled' do
+      action :disable
+    end
   end
 
-  context 'with remote management already enabled and configured' do
-    before do
-      allow(File).to receive(:exist?).and_call_original
-      allow(File).to receive(:exist?)
-        .with('/Library/Application Support/Apple/Remote Desktop/RemoteManagement.launchd')
-        .and_return(true)
-      allow_any_instance_of(RemoteManagement).to receive(:plist)
-        .and_return 'Dict { ARD_AllLocalUsersPrivs = 1073742079
-                                allowInsecureDH = true
-                                ARD_AllLocalUsers = true }'
-    end
+  it_behaves_like 'kickstart not activating or configuring the ARD agent'
+  it_behaves_like 'kickstart not deactivating or stopping the ARD agent'
+end
 
-    recipe do
-      remote_management 'disabled' do
-        action :disable
-      end
-    end
+describe 'disabling when already enabled' do
+  include_context 'with remote management enabled'
 
-    it { is_expected.to run_execute('/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate') }
-    it { is_expected.to run_execute('/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -stop') }
+  recipe do
+    remote_management 'disabled' do
+      action :disable
+    end
   end
+
+  it_behaves_like 'kickstart not activating or configuring the ARD agent'
+  it_behaves_like 'kickstart deactivating and stopping the ARD agent'
 end
