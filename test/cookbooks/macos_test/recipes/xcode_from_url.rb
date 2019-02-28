b5f7cf8141d146cc7d831049300954104d33c4ec
Jacob Zaval <jazava@microsoft.com>
2019-02-28 10:20:47 -0800

    add '9.2' version to sierra url install

--- test/cookbooks/macos_test/recipes/xcode_from_url.rb
+++ test/cookbooks/macos_test/recipes/xcode_from_url.rb
@@ -1,4 +1,12 @@
-xcode 'installs 10.1' do
-  download_url node['xcode']['download_url']
-  version '10.1'
+if mac_os_x_after_sierra?
+  xcode 'installs 10.1' do
+    download_url node['xcode']['download_url']
+    version '10.1'
+  end
+
+elsif mac_os_x_sierra?
+  xcode 'installs 10.1' do
+    download_url node['xcode']['download_url']
+    version '9.2'
+  end
 end
