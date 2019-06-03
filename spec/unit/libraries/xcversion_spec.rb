e1795a80e4576e5c2079b0e992a57a0d33fbeae6
Eric Hanko <v-erhank@microsoft.com>
2019-06-03 13:44:46 -0700

    Glorious TDD

--- spec/unit/libraries/xcversion_spec.rb
+++ spec/unit/libraries/xcversion_spec.rb
@@ -12,10 +12,17 @@ describe MacOS::XCVersion do
   context 'when given an Xcode object without a download url' do
     before do
       allow(MacOS::XCVersion).to receive(:available_versions).and_return(["10 GM seed\n"])
+      allow(MacOS::XCVersion).to receive(:xcversion_path).and_return('/foo/bar/bin/xcversion')
     end
-    it 'returns the appropriate --url syntax' do
+
+    it 'passes an empty string for the url parameter' do
       xcode = MacOS::Xcode.new('10.0', '/Applications/Xcode.app', '')
       expect(XCVersion.download_url_option(xcode)).to eq ''
+    end
+
+    it 'executes the correct xcversion command with quotes around the version' do
+      xcode = MacOS::Xcode.new('10.0', '/Applications/Xcode.app', '')
+      expect(XCVersion.install_xcode(xcode)).to eq "/foo/bar/bin/xcversion install '10 GM seed'"
     end
   end
 end
