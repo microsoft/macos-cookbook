39bca3e74ad4c811bc4685a75ba39cc171171ca8
Eric Hanko <v-erhank@microsoft.com>
2019-06-03 13:48:49 -0700

    Glorious TDD - Pt. 2

--- libraries/xcversion.rb
+++ libraries/xcversion.rb
@@ -42,7 +42,7 @@ module MacOS
       end
 
       def install_xcode(xcode)
-        xcversion "install #{xcode.version} #{download_url_option(xcode)}"
+        xcversion "install '#{xcode.version}' #{download_url_option(xcode)}".strip
       end
 
       def installed_xcodes
