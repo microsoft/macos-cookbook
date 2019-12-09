4992613da8f6120af90d18b5f875fdc9f9eb5990
Jacob Zaval <jazava@microsoft.com>
2019-12-09 10:49:45 -0800

    add a new test for upgrade action

--- spec/unit/resources/command_line_tools_spec.rb
+++ spec/unit/resources/command_line_tools_spec.rb
@@ -38,3 +38,32 @@ describe 'command_line_tools' do
     it { is_expected.to_not run_execute('install Command Line Tools (macOS High Sierra version 10.13) for Xcode-10.0') }
   end
 end
+
+describe 'command_line_tools' do
+  step_into :command_line_tools
+  platform 'mac_os_x'
+
+  context 'with a different version of command line tools than currently installed available' do
+    before do
+      allow_any_instance_of(MacOS::CommandLineTools).to receive(:softwareupdate_list)
+        .and_return(["Software Update Tool\n",
+                     "\n", "Finding available software\n",
+                     "Software Update found the following new or updated software:\n",
+                     "* Label: Command Line Tools for Xcode-22.0\n",
+                     "\tTitle: Command Line Tools for Xcode, Version: 22.0, Size: 224868K, Recommended: YES, \n"])
+      allow_any_instance_of(MacOS::CommandLineTools).to receive(:install_history_plist)
+        .and_return('spec/unit/libraries/data/InstallHistory_with_CLT.plist')
+    end
+
+    recipe do
+      command_line_tools 'always latest' do
+        action :upgrade
+      end
+    end
+
+    it {
+      is_expected.to run_execute('upgrade Command Line Tools for Xcode-11.0')
+        .with(command: ['softwareupdate', '--install', 'Command Line Tools for Xcode-22.0'])
+    }
+  end
+end
