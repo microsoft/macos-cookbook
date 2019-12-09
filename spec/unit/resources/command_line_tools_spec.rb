2bdff39a6fc06941e0102b15d144855f268b4829
Jacob Zaval <jazava@microsoft.com>
2019-12-09 10:44:55 -0800

    refactor existing tests now that we have two actions

--- spec/unit/resources/command_line_tools_spec.rb
+++ spec/unit/resources/command_line_tools_spec.rb
@@ -11,24 +11,28 @@ describe 'command_line_tools' do
   end
 
   context 'without libxcrun present' do
-    before(:each) do
+    before do
       allow(File).to receive(:exist?).with('/Library/Developer/CommandLineTools/usr/lib/libxcrun.dylib').and_return(false)
     end
 
     recipe do
-      command_line_tools 'horse'
+      command_line_tools 'horse' do
+        action :install
+      end
     end
 
     it { is_expected.to run_execute('install Command Line Tools (macOS High Sierra version 10.13) for Xcode-10.0') }
   end
 
   context 'with libxcrun present' do
-    before(:each) do
+    before do
       allow(File).to receive(:exist?).with('/Library/Developer/CommandLineTools/usr/lib/libxcrun.dylib').and_return(true)
     end
 
     recipe do
-      command_line_tools 'no_description'
+      command_line_tools 'horse' do
+        action :install
+      end
     end
 
     it { is_expected.to_not run_execute('install Command Line Tools (macOS High Sierra version 10.13) for Xcode-10.0') }
