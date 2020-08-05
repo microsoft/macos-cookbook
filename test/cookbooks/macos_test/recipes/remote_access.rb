9fbd4c23540415681a2caacbf519167a58a129d1
Jacob Zaval <jazava@microsoft.com>
2020-08-05 11:18:05 -0700

    skip remote access for beta Big Sur

--- test/cookbooks/macos_test/recipes/remote_access.rb
+++ test/cookbooks/macos_test/recipes/remote_access.rb
@@ -1,3 +1,4 @@
 remote_management 'activate and configure remote management for all users' do
   action :enable
+  not_if { node['platform_version'] >= '11.0' } # remove prior to Big Sur public release
 end
