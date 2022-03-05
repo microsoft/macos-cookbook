3841a0a5b42ab9c4734e036c02c27bca02cb6129
Jacob Zaval <jazava@microsoft.com>
2022-03-04 19:49:50 -0800

    remove last default attributes

--- attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default['macos']['admin_user'] = 'vagrant'
-default['macos']['admin_password'] = 'vagrant'
--- test/cookbooks/macos_test/recipes/certificate.rb
+++ test/cookbooks/macos_test/recipes/certificate.rb
@@ -27,7 +27,7 @@ end
 certificate 'install a .cer format certificate file' do
   certfile foobar_cer_path
   keychain '/Users/vagrant/Library/Keychains/login.keychain'
-  kc_passwd node['macos']['admin_password']
+  kc_passwd 'test'
   apps ['/Applications/Numbers.app']
   action :install
 end
