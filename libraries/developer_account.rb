2fa381da9ef9de060853440708eb57eba14fa14e
Jacob Zaval <jazava@microsoft.com>
2022-03-07 14:08:14 -0800

    update tests to take env vars from kitchen lifecycle

--- libraries/developer_account.rb
+++ libraries/developer_account.rb
@@ -7,11 +7,28 @@ module MacOS
 
     def initialize(apple_id_hash, download_url)
       if download_url.empty?
+        developer_id = validate_apple_id(apple_id_hash)
         @credentials = {
-          XCODE_INSTALL_USER: apple_id_hash[:username],
-          XCODE_INSTALL_PASSWORD: apple_id_hash[:password],
+          XCODE_INSTALL_USER: developer_id[:apple_id],
+          XCODE_INSTALL_PASSWORD: developer_id[:password],
         }
         authenticate_with_apple(@credentials)
+      end
+    end
+
+    def validate_apple_id(apple_id_hash)
+      if apple_id_hash.values.any?
+        {
+          apple_id: apple_id_hash[:username],
+          password: apple_id_hash[:password],
+        }
+      elsif ENV['XCODE_INSTALL_USER'] && ENV['XCODE_INSTALL_PASSWORD']
+        {
+          apple_id: ENV['XCODE_INSTALL_USER'],
+          password: ENV['XCODE_INSTALL_PASSWORD'],
+        }
+      else
+        raise('Developer credentials not supplied, and a URL was not provided for Xcode!')
       end
     end
 
