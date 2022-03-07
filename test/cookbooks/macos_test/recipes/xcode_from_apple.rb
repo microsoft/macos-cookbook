8606e0ed9836acb7fdb5c7e78c305217427b9301
Jacob Zaval <jazava@microsoft.com>
2022-03-07 15:09:32 -0800

    update suites to write to local files

--- test/cookbooks/macos_test/recipes/xcode_from_apple.rb
+++ test/cookbooks/macos_test/recipes/xcode_from_apple.rb
@@ -1,10 +1,10 @@
 if node['platform_version'] >= '10.15.6'
   xcode '12.0' do
-    apple_id({ username: ENV['APPLEID_USERNAME'], password: ENV['APPLEID_PASSWORD'] })
+    apple_id({ username: ::File.read('/Users/vagrant/username'), password: ::File.read('/Users/vagrant/password') })
   end
 else
   xcode '10.2.1' do
     ios_simulators ['12', '11']
-    apple_id({ username: ENV['APPLEID_USERNAME'], password:  ENV['APPLEID_PASSWORD'] })
+    apple_id({ username: ::File.read('/Users/vagrant/username'), password: ::File.read('/Users/vagrant/password') })
   end
 end
--- test/cookbooks/macos_test/recipes/xcode_from_url.rb
+++ test/cookbooks/macos_test/recipes/xcode_from_url.rb
@@ -1,11 +1,11 @@
 if node['platform_version'] >= '10.15.6'
   xcode 'installs 12.0' do
-    download_url ENV['XCODE_URL']
+    download_url ::File.read('/Users/vagrant/xcode')
     version '12.0'
   end
 else
   xcode 'installs 10.2.1' do
-    download_url ENV['XCODE_URL']
+    download_url ::File.read('/Users/vagrant/xcode')
     version '10.2.1'
   end
 end
