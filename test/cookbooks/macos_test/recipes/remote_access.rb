ab050cb837c7b4aa2060dc98b522b182e353a963
Jared Weyer <v-jaredweyer@microsoft.com>
2023-03-09 09:34:06 -0800

    update remote management test recipe to use new remote managment resource properties. Add logic for updating TCC db

--- test/cookbooks/macos_test/recipes/remote_access.rb
+++ test/cookbooks/macos_test/recipes/remote_access.rb
@@ -1,4 +1,20 @@
+tcc_db_path = '/Library/Application Support/com.apple.TCC/TCC.db'
+
+execute 'authorize screensharing client to utilize the kTCCServicePostEvent service' do
+  command ['/usr/bin/sqlite3', tcc_db_path, "INSERT OR REPLACE INTO access VALUES('kTCCServicePostEvent','com.apple.screensharing.agent',0,2,4,1,NULL,NULL,0,'UNUSED',NULL,0,1639743960);" ]
+  not_if { RemoteManagement.correct_tcc_db_privileges? }
+  only_if { shell_out('/usr/sbin/system_profiler', 'SPSoftwareDataType').stdout.match?(Regexp.new('System Integrity Protection: Disabled')) }
+end
+
+execute 'authorize screensharing client to utilize the kTCCServiceScreenCapture service' do
+  command ['/usr/bin/sqlite3', tcc_db_path, "INSERT OR REPLACE INTO access VALUES ('kTCCServiceScreenCapture','com.apple.screensharing.agent',0,2,4,1,NULL,NULL,0,'UNUSED',NULL,0,1639743960);" ]
+  not_if { RemoteManagement.correct_tcc_db_privileges? }
+  only_if { shell_out('/usr/sbin/system_profiler', 'SPSoftwareDataType').stdout.match?(Regexp.new('System Integrity Protection: Disabled')) }
+end
+
 remote_management 'activate and configure remote management for all users' do
-  action :enable
-  not_if { node['platform_version'] >= '11.0' } # remove prior to Big Sur public release
+  users 'vagrant'
+  privileges %w(control_observe open_quit_apps)
+  computer_info ['Arkenstone', 'Gold']
+  only_if { shell_out('/usr/sbin/system_profiler', 'SPSoftwareDataType').stdout.match?(Regexp.new('System Integrity Protection: Disabled')) }
 end
