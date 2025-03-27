29f19a674b9fd8684013d2c86825c80360acf446
Jesse Bullard <jessebullard1996@gmail.com>
2025-03-27 13:56:29 -0700

    Bump cookbook version to 6.0.4
    Signed-off-by: Jesse Bullard <jessebullard1996@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@microsoft.com'
 license 'MIT'
 description 'Resources for configuring and provisioning macOS'
 chef_version '>= 16.0'
-version '6.0.3'
+version '6.0.4'
 
 source_url 'https://github.com/Microsoft/macos-cookbook'
 issues_url 'https://github.com/Microsoft/macos-cookbook/issues'
