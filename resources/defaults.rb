2ec704b795a3bba62051301029d2a7c4a10eb3d2
Jacob Zaval <jazava@microsoft.com>
2025-05-08 23:01:46 -0700

    use macos path

--- resources/defaults.rb
+++ resources/defaults.rb
@@ -41,7 +41,7 @@ action :run do
     execute "#{setting} to #{value}" do
       command "/usr/bin/defaults #{new_resource.option} #{Shellwords.shellescape(new_resource.domain)} #{Shellwords.shellescape(setting)} #{value}"
       user new_resource.user
-      environment ({ 'HOME' => Etc.getpwnam(new_resource.user).dir })
+      environment({ 'HOME' => MacOS::UserPath.home(new_resource.user) })
     end
   end
 end
